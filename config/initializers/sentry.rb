06882936beef25a4490cd7aca57f67c8546cba8e
alice-carr <alice.bc.carr@gmail.com>
2022-04-05 15:05:14 +0100

    initialise sentry with basic setup relates #3

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,19 @@
+require 'dotenv/load'
+
+Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+    config.debug = true
+    config.traces_sample_rate = 1.0
+  end
+
+# Uncomment out the below to test Sentry - this
+# will raise 2 issues in Sentry
+
+# begin
+#   1 / 0
+# rescue ZeroDivisionError => exception
+#   Sentry.capture_exception(exception)
+# end
+
+# Sentry.capture_message("test message")
