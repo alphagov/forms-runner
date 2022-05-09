ecc51be69e679392663ce0cf9a3379c80055a34a
alice-carr <alice.bc.carr@gmail.com>
2022-05-09 11:32:19 +0100

    more linting

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -5,21 +5,21 @@ class NotifyService
     @notify_api_key = ENV['NOTIFY_API_KEY']
   end
 
-  def test_email(email_address)
-      unless @notify_api_key
-        Rails.logger.warn 'Warning: no NOTIFY_API_KEY set.'
-        return nil
-      end
-  
-      client = Notifications::Client.new(@notify_api_key)
-      client.send_email(
+  def send_test_email(email_address)
+    unless @notify_api_key
+      Rails.logger.warn 'Warning: no NOTIFY_API_KEY set.'
+      return nil
+    end
+
+    client = Notifications::Client.new(@notify_api_key)
+    client.send_email(
           email_address: email_address,
-          template_id: 'f33517ff-2a88-4f6e-b855-c550268ce08a',
-          personalisation: {
-          }
-      )
+        template_id: 'f33517ff-2a88-4f6e-b855-c550268ce08a',
+        personalisation: {
+        }
+    )
   end
 end
 
 notify_service = NotifyService.new
-notify_service.test_email('email@email.com')
+notify_service.send_test_email('email@email.com')
