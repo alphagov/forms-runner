85dedbd90552dd1a6a478a9f985b756c233845a1
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-07-11 16:34:13 +0100

    Improve email pages
    Stick with simple @ validation for now.

--- spec/models/question/email_spec.rb
+++ spec/models/question/email_spec.rb
@@ -4,15 +4,38 @@ require "shared_examples_for_question_models"
 RSpec.describe Question::Email, type: :model do
   it_behaves_like "a question model"
 
-  it "validates presence" do
-    question = described_class.new
-    question.email = ""
-    question.validate
-    expect(question.errors[:email]).to include("can't be blank")
+  context "when given an empty string or nil" do
+    it "returns invalid with blank message" do
+      question = described_class.new
+      question.validate
+      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
+      question.email = ""
+      question.validate
+      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
+      expect(question.errors[:email]).not_to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
+    end
 
-    question.email = "email address"
-    question.validate
-    expect(question.errors[:email]).not_to include("can't be blank")
-    expect(question).to be_valid
+    it "shows as a blank string" do
+      question = described_class.new
+      expect(question.show_answer).to eq ""
+    end
+  end
+
+  context "when given a string with an @ symbol in" do
+    it "validates" do
+      question = described_class.new
+      question.email = " @ "
+      question.validate
+      expect(question).to be_valid
+    end
+  end
+
+  context "when given a string without an @ symbol in" do
+    it "does not validate an address without an @" do
+      question = described_class.new
+      question.email = "not an email address"
+      question.validate
+      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
+    end
   end
 end
