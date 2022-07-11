3d2e994c8458ca97b2efbfa2b303e13fca76993e
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-07-11 16:34:13 +0100

    Improve email pages
    Stick with simple @ validation for now.

--- app/models/question/email.rb
+++ app/models/question/email.rb
@@ -1,4 +1,11 @@
 class Question::Email < Question::QuestionBase
+  # see https://design-system.service.gov.uk/patterns/email-addresses/
+  # This model contains very lite checking - only that an @ exists. Pay and
+  # notify both have good examples of better email validation checks and ways
+  # to help users enter the right value
+
+  EMAIL_REGEX = /.*@.*/
   attribute :email
   validates :email, presence: true
+  validates :email, format: { with: EMAIL_REGEX, message: :invalid_email }, allow_blank: true
 end
