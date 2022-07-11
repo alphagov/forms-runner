8d5968f47c3b929347abb004bc01fafdb8ca565f
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-07-11 16:27:43 +0100

    Improve single_line pages

--- spec/models/question/single_line_spec.rb
+++ spec/models/question/single_line_spec.rb
@@ -4,15 +4,19 @@ require "shared_examples_for_question_models"
 RSpec.describe Question::SingleLine, type: :model do
   it_behaves_like "a question model"
 
-  it "validates presence" do
-    question = described_class.new
-    question.text = ""
-    question.validate
-    expect(question.errors[:text]).to include("can't be blank")
+  context "when given an empty string or nil" do
+    it "returns invalid with blank message" do
+      question = described_class.new
+      question.validate
+      expect(question.errors[:text]).to include(I18n.t("activemodel.errors.models.question/single_line.attributes.text.blank"))
+      question.text = ""
+      question.validate
+      expect(question.errors[:text]).to include(I18n.t("activemodel.errors.models.question/single_line.attributes.text.blank"))
+    end
 
-    question.text = "text"
-    question.validate
-    expect(question.errors[:text]).not_to include("can't be blank")
-    expect(question).to be_valid
+    it "shows as a blank string" do
+      question = described_class.new
+      expect(question.show_answer).to eq ""
+    end
   end
 end
