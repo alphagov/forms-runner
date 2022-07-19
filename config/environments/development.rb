e136d3a0be7f5a1a4674585fcc696602472ea95c
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-07-19 16:38:33 +0100

    Remove database
    The project does not use the postgres DB configured as part of the rails
    setup. The database is complicates dev, testing and deployment.
    
    The database config has been removed:
     - configuration and env files
     - rails dependencies on active_record and active_storage (which we also
       don't use)
     - setup scripts and github actions
     - docker compose files
     - the PAAS manifest
     - readme

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -50,17 +47,11 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
-
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
