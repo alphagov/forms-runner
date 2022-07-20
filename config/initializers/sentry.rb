ae4984cfb67879ace00bc848fec5cac79aaad92d
Tristram Oaten <tris@oat.sh>
2022-07-20 12:09:31 +0100

    Fix breadcrums logger
    Co-authored-by: James Sheppard <james-sheppard-gov-uk@users.noreply.github.com>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = %i[sentry_logger http_logger]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.debug = true
   config.traces_sample_rate = 1.0
 end
