c64873b6e09c2482da7b3d3421feef9bb4b5d094
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-08-17 16:30:21 +0100

    Fix Sentry and add environment (#86)
    * Fix missing sentry variable
    
    * Add sentry environment
    
    * Send handled exceptions to Sentry
    
    * Fix linting
    
    * Use PAAS_ENVIRONMENT var
    
    * Add missing environment variable

--- app/controllers/forms/submit_answers_controller.rb
+++ app/controllers/forms/submit_answers_controller.rb
@@ -5,7 +5,8 @@ module Forms
       submit_form(answers)
       current_context.clear
       redirect_to :form_submitted
-    rescue StandardError
+    rescue StandardError => e
+      Sentry.capture_exception(e)
       render "errors/submission_error", status: :internal_server_error
     end
 
