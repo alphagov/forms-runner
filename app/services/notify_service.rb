711d1316d56f2b9e0dbe9932d353b85e525310cf
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-08-30 13:30:22 +0100

    Update formatting of form submission email

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -5,20 +5,25 @@ class NotifyService
     @notify_api_key = ENV["NOTIFY_API_KEY"]
   end
 
-  def send_email(email_address, title, text_input, submission_time)
+  def send_email(email_address, title, text_input)
     unless @notify_api_key
       Rails.logger.warn "Warning: no NOTIFY_API_KEY set."
       return nil
     end
 
+    timestamp = Time.now.zone
+    submission_time = timestamp.strftime("%H:%M:%S")
+    submission_date = timestamp.strftime("%-d %B %Y")
+
     client = Notifications::Client.new(@notify_api_key)
     client.send_email(
       email_address:,
-      template_id: "c10a4cea-b937-4f9e-be6f-f18583e55806",
+      template_id: "427eb8bc-ce0d-40a3-bf54-d76e8c3ec916",
       personalisation: {
         title:,
         text_input:,
         submission_time:,
+        submission_date:,
       },
     )
   end
