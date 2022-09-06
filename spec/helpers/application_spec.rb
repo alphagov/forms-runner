3cd5ca4f0cd5f02d1005cf5e7f2c6222347356a0
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-06 15:47:11 +0100

    Fix linting

--- spec/helpers/application_spec.rb
+++ spec/helpers/application_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe ApplicationHelper, :type => :helper do
+RSpec.describe ApplicationHelper, type: :helper do
   describe "page_title" do
     it "returns the title with the GOV.UK suffix" do
       helper.set_page_title("Test")
