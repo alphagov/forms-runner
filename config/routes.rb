efb8d6ac3f009500ace5622e3375ebf625536bc4
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-15 14:48:50 +0100

    Use concerns to reduce repetition

--- config/routes.rb
+++ config/routes.rb
@@ -9,24 +9,22 @@ Rails.application.routes.draw do
   get "/help/accessibility-statement" => "help#accessibility_statement", as: :accessibility_statement
   get "/help/cookies" => "help#cookies", as: :cookies
 
+  concern :previewable_routes do |options|
+    get "/:id" => "form#show", as: "#{options[:prefix]}form"
+    get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: "#{options[:prefix]}check_your_answers"
+    post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: "#{options[:prefix]}form_submit_answers"
+    get "/:form_id/submitted" => "forms/submitted#submitted", as: "#{options[:prefix]}form_submitted"
+    get "/:form_id/:page_slug/change" => "forms/page#show", as: "#{options[:prefix]}form_change_answer", defaults: { changing_existing_answer: true }
+    get "/:form_id/:page_slug" => "forms/page#show", as: "#{options[:prefix]}form_page"
+    post "/:form_id/:page_slug" => "forms/page#save", as: "#{options[:prefix]}save_form_page"
+  end
+
   scope "/form" do
-    get "/:id" => "form#show", as: :form
-    get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: :check_your_answers
-    post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: :form_submit_answers
-    get "/:form_id/submitted" => "forms/submitted#submitted", as: :form_submitted
-    get "/:form_id/:page_slug/change" => "forms/page#show", as: :form_change_answer, defaults: { changing_existing_answer: true }
-    get "/:form_id/:page_slug" => "forms/page#show", as: :form_page
-    post "/:form_id/:page_slug" => "forms/page#save", as: :save_form_page
+    concerns :previewable_routes
   end
 
   scope "/preview-form", defaults: { preview: true } do
-    get "/:id" => "form#show", as: :preview_form
-    get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: :preview_check_your_answers
-    post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: :preview_form_submit_answers
-    get "/:form_id/submitted" => "forms/submitted#submitted", as: :preview_form_submitted
-    get "/:form_id/:page_slug/change" => "forms/page#show", as: :preview_form_change_answer, defaults: { changing_existing_answer: true }
-    get "/:form_id/:page_slug" => "forms/page#show", as: :preview_form_page
-    post "/:form_id/:page_slug" => "forms/page#save", as: :preview_save_form_page
+    concerns :previewable_routes, { prefix: "preview_" }
   end
 
   get "/404", to: "errors#not_found", as: :error_404, via: :all
