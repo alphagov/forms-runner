93ae0f3a58162a9a128134b43198c659f1fb67a2
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-15 14:53:05 +0100

    Clean up prefix interpolation

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,15 @@ Rails.application.routes.draw do
   get "/help/cookies" => "help#cookies", as: :cookies
 
   concern :previewable_routes do |options|
-    get "/:id" => "form#show", as: "#{options[:prefix]}form"
-    get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: "#{options[:prefix]}check_your_answers"
-    post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: "#{options[:prefix]}form_submit_answers"
-    get "/:form_id/submitted" => "forms/submitted#submitted", as: "#{options[:prefix]}form_submitted"
-    get "/:form_id/:page_slug/change" => "forms/page#show", as: "#{options[:prefix]}form_change_answer", defaults: { changing_existing_answer: true }
-    get "/:form_id/:page_slug" => "forms/page#show", as: "#{options[:prefix]}form_page"
-    post "/:form_id/:page_slug" => "forms/page#save", as: "#{options[:prefix]}save_form_page"
+    prefix = options[:prefix] ? "#{options[:prefix]}_" : ""
+
+    get "/:id" => "form#show", as: "#{prefix}form"
+    get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: "#{prefix}check_your_answers"
+    post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: "#{prefix}form_submit_answers"
+    get "/:form_id/submitted" => "forms/submitted#submitted", as: "#{prefix}form_submitted"
+    get "/:form_id/:page_slug/change" => "forms/page#show", as: "#{prefix}form_change_answer", defaults: { changing_existing_answer: true }
+    get "/:form_id/:page_slug" => "forms/page#show", as: "#{prefix}form_page"
+    post "/:form_id/:page_slug" => "forms/page#save", as: "#{prefix}save_form_page"
   end
 
   scope "/form" do
@@ -24,7 +26,7 @@ Rails.application.routes.draw do
   end
 
   scope "/preview-form", defaults: { preview: true } do
-    concerns :previewable_routes, { prefix: "preview_" }
+    concerns :previewable_routes, { prefix: "preview" }
   end
 
   get "/404", to: "errors#not_found", as: :error_404, via: :all
