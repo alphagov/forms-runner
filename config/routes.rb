9e32fccb86541a1ec90f5a6a9cd5d30d1d5de57d
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-20 15:16:58 +0100

    Add privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     get "/:form_id/check_your_answers" => "forms/check_your_answers#show", as: :check_your_answers
     post "/:form_id/submit_answers" => "forms/submit_answers#submit_answers", as: :form_submit_answers
     get "/:form_id/submitted" => "forms/submitted#submitted", as: :form_submitted
+    get "/:form_id/privacy" => "forms/privacy_page#show", as: :form_privacy
     get "/:form_id/:page_slug/change" => "forms/page#show", as: :form_change_answer, defaults: { changing_existing_answer: true }
     get "/:form_id/:page_slug" => "forms/page#show", as: :form_page
     post "/:form_id/:page_slug" => "forms/page#save", as: :save_form_page
