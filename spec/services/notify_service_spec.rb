e1135cdb8110c964da6cb7c5ae35e402eb9cf976
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-09-20 14:45:03 +0100

    Add a test submission with datetime in GMT

--- spec/services/notify_service_spec.rb
+++ spec/services/notify_service_spec.rb
@@ -10,25 +10,50 @@ RSpec.describe NotifyService do
 
   context "with api key set" do
     let(:notify_api_key) { 'test-key' }
-    let(:submission_datetime) { Time.utc(2022, 9, 14, 10, 00, 00) }
 
-    it "sends correct values to notify" do
-      fake_notify_client = instance_double(Notifications::Client)
-      allow(fake_notify_client).to receive(:send_email)
-      allow(Notifications::Client).to receive(:new).and_return(fake_notify_client)
+    context "with a time in BST" do
+      let(:submission_datetime) { Time.utc(2022, 9, 14, 10, 00, 00) }
+      it "sends correct values to notify" do
+        fake_notify_client = instance_double(Notifications::Client)
+        allow(fake_notify_client).to receive(:send_email)
+        allow(Notifications::Client).to receive(:new).and_return(fake_notify_client)
 
-      travel_to submission_datetime do
-        notify_service = NotifyService.new
-        notify_service.send_email('fake-email','title','text')
-        expect(fake_notify_client).to have_received(:send_email).with(
-          {:email_address=>'fake-email',
-           :personalisation=>{
-             :submission_date=>"14 September 2022",
-             :submission_time=>"11:00:00",
-             :text_input=>'text',
-             :title=>'title'
-           },
-           :template_id=>"427eb8bc-ce0d-40a3-bf54-d76e8c3ec916"}).once
+        travel_to submission_datetime do
+          notify_service = NotifyService.new
+          notify_service.send_email('fake-email','title','text')
+          expect(fake_notify_client).to have_received(:send_email).with(
+            {:email_address=>'fake-email',
+             :personalisation=>{
+               :submission_date=>"14 September 2022",
+               :submission_time=>"11:00:00",
+               :text_input=>'text',
+               :title=>'title'
+             },
+             :template_id=>"427eb8bc-ce0d-40a3-bf54-d76e8c3ec916"}).once
+        end
+      end
+    end
+
+    context "with a time in GMT" do
+      let(:submission_datetime) { Time.utc(2022, 12, 14, 10, 00, 00) }
+      it "sends correct values to notify" do
+        fake_notify_client = instance_double(Notifications::Client)
+        allow(fake_notify_client).to receive(:send_email)
+        allow(Notifications::Client).to receive(:new).and_return(fake_notify_client)
+
+        travel_to submission_datetime do
+          notify_service = NotifyService.new
+          notify_service.send_email('fake-email','title','text')
+          expect(fake_notify_client).to have_received(:send_email).with(
+            {:email_address=>'fake-email',
+             :personalisation=>{
+               :submission_date=>"14 December 2022",
+               :submission_time=>"10:00:00",
+               :text_input=>'text',
+               :title=>'title'
+             },
+             :template_id=>"427eb8bc-ce0d-40a3-bf54-d76e8c3ec916"}).once
+        end
       end
     end
   end
