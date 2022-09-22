9d298b549765d87a09e206d99b8585a5405e7290
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-09-22 20:02:30 +0100

    Refactor application based endpoints
    It seemed pointless having an empty controller floating around which
    served as a placeholder for the two routes that were really just part
    of the overall application.
    
    I know that rails is clever enough for us not to define an action in
    in the controller if the action name and view name are the same and
    it doesn't need to set any vars etc. but this might confuse someone
    who isn't familar with rails and this sort of pattern.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :check_service_unavailable
   after_action :add_robots_header
 
+  def accessibility_statement; end
+
+  def cookies; end
+
   def check_service_unavailable
     if ENV["SERVICE_UNAVAILABLE"].present?
       render "errors/service_unavailable", status: :service_unavailable, formats: :html
