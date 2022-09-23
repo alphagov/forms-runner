db7c90a3df94dae82fec76796d25a4f8a032669d
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-09-23 09:30:24 +0100

    Remove the page for forms with no start_page
    Currently, when a user visits /form/:formid/ they are redirected to
    /form/:formid/:form_slug . When a user visits or
    /form/:formid/:form_slug they are redirected to the start_page of the
    form, or shown a page which says there are no pages for this form.
    
    This commit merges both behaviors. Now a user hitting either route will
    be directed to the start_page. If the form does not have a start_page,
    they are given a 404.

--- app/controllers/forms/base_controller.rb
+++ app/controllers/forms/base_controller.rb
@@ -1,26 +1,31 @@
 module Forms
   class BaseController < ApplicationController
+    def redirect_to_user_friendly_url
+      redirect_to_friendly_url_start
+    end
+
+    def show
+      redirect_to_friendly_url_start
+    end
 
     rescue_from ActiveResource::ResourceNotFound do
       render template: "errors/not_found", status: :not_found
     end
 
-    def redirect_to_user_friendly_url
-      @form = Form.find(params.require(:form_id))
-      redirect_to form_path(@form.id, @form.form_slug)
-    end
+  private
 
-    def show
+    def redirect_to_friendly_url_start
+      # this will cleaned up in the PR which adds a guard to return 404 for all forms which either:
+      # don't have a start page or
+      # don't have a live_at date in the past outside of preview
       @form = Form.find(params.require(:form_id))
       if @form.start_page
         redirect_to form_page_path(params.require(:form_id), @form.form_slug, @form.start_page)
-        unless preview?
-          EventLogger.log_form_event(Context.new(form: @form, store: session), request, "visit")
-        end
+        EventLogger.log_form_event(Context.new(form: @form, store: session), request, "visit") unless preview?
+      else
+        raise ActiveResource::ResourceNotFound, "no start page"
       end
     end
-
-    private
 
     def current_context
       @current_context ||= Context.new(form: Form.find(params.require(:form_id)), store: session)
