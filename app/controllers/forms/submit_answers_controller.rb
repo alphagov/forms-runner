b29c07cb0410cd7ed8dd735b3b4d957307b40a31
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-10-12 12:08:10 +0100

    Refactor submit_answers_controller
    Moved the email formatting to NotifyService as it feels like
    its closely related to functionality we would only use when
    sending emails from notify. It also makes it easier for us
    to test our changes.
    
    Co-authored-by: David Biddle <david.biddle@digital.cabinet-office.gov.uk>
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
    
    Fix refactor

--- app/controllers/forms/submit_answers_controller.rb
+++ app/controllers/forms/submit_answers_controller.rb
@@ -4,23 +4,13 @@ module Forms
       unless preview?
         EventLogger.log_form_event(current_context, request, "submission")
       end
-      submit_form(answers)
+
+      NotifyService.new.send_email(current_context, preview_mode: preview?)
       current_context.clear
       redirect_to :form_submitted
     rescue StandardError => e
       Sentry.capture_exception(e)
       render "errors/submission_error", status: :internal_server_error
-    end
-
-  private
-
-    def submit_form(text)
-      # in the controller for now but can be moved to service object, maybe use actionmailer fo easier testing?
-      NotifyService.new.send_email(current_context.submission_email, current_context.form_name, text, preview_mode: preview?)
-    end
-
-    def answers
-      current_context.steps.map { |page| "# #{page.question_text}\n#{page.show_answer}" }.join("\n\n---\n\n")
     end
   end
 end
