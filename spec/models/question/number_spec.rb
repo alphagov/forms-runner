1d5aa01a0d8e13c3d0930db47cb404b76c648c35
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-10-14 14:37:02 +0100

    Remove redundant calls to validate (#160)
    * Remove redundant calls to validate
    
    * Refactor number spec to follow other question model specs
    
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- spec/models/question/number_spec.rb
+++ spec/models/question/number_spec.rb
@@ -2,44 +2,44 @@ require "rails_helper"
 require "shared_examples_for_question_models"
 
 RSpec.describe Question::Number, type: :model do
+  let(:question) { described_class.new }
+
   it_behaves_like "a question model"
 
   context "when given an empty string or nil" do
     it "returns invalid with blank message" do
-      question = described_class.new
-      question.validate
+      expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.blank"))
+    end
+
+    it "returns invalid with empty string" do
       question.number = ""
-      question.validate
+      expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.blank"))
     end
 
     it "shows as a blank string" do
-      question = described_class.new
       expect(question.show_answer).to eq ""
     end
   end
 
   context "when given a whole number" do
     it "validates without errors" do
-      question = described_class.new(number: "299792458")
-      question.validate
+      question.number = "299792458"
       expect(question).to be_valid
     end
   end
 
   context "when given a decimal number" do
     it "validates without errors" do
-      question = described_class.new(number: "8.5")
-      question.validate
+      question.number = "8.5"
       expect(question).to be_valid
     end
   end
 
   context "when given a negative number" do
     it "returns a validation error" do
-      question = described_class.new(number: "-1")
-      question.validate
+      question.number = "-1"
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.greater_than_or_equal_to"))
     end
@@ -47,8 +47,7 @@ RSpec.describe Question::Number, type: :model do
 
   context "when given a string which is not numeric" do
     it "returns a validation error" do
-      question = described_class.new(number: "two")
-      question.validate
+      question.number = "two"
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.not_a_number"))
     end
