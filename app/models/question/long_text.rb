36d195afd4ba9bbbeaf6ce2b464c11cc2d16f670
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-10-14 14:50:28 +0100

    Make long text question type optional

--- app/models/question/long_text.rb
+++ app/models/question/long_text.rb
@@ -1,7 +1,7 @@
 module Question
   class LongText < QuestionBase
     attribute :text
-    validates :text, presence: true
+    validates :text, presence: true, unless: :is_optional?
     validates :text, length: { maximum: 5000 }
   end
 end
