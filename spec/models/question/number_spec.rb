43032ec31e20bcaa494ea9341bb4ec3bd399bc73
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-10-14 14:31:57 +0100

    Refactor number spec to follow other question model specs

--- spec/models/question/number_spec.rb
+++ spec/models/question/number_spec.rb
@@ -2,41 +2,44 @@ require "rails_helper"
 require "shared_examples_for_question_models"
 
 RSpec.describe Question::Number, type: :model do
+  let(:question) { described_class.new }
+
   it_behaves_like "a question model"
 
   context "when given an empty string or nil" do
     it "returns invalid with blank message" do
-      question = described_class.new
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.blank"))
+    end
+
+    it "returns invalid with empty string" do
       question.number = ""
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.blank"))
     end
 
     it "shows as a blank string" do
-      question = described_class.new
       expect(question.show_answer).to eq ""
     end
   end
 
   context "when given a whole number" do
     it "validates without errors" do
-      question = described_class.new(number: "299792458")
+      question.number = "299792458"
       expect(question).to be_valid
     end
   end
 
   context "when given a decimal number" do
     it "validates without errors" do
-      question = described_class.new(number: "8.5")
+      question.number = "8.5"
       expect(question).to be_valid
     end
   end
 
   context "when given a negative number" do
     it "returns a validation error" do
-      question = described_class.new(number: "-1")
+      question.number = "-1"
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.greater_than_or_equal_to"))
     end
@@ -44,7 +47,7 @@ RSpec.describe Question::Number, type: :model do
 
   context "when given a string which is not numeric" do
     it "returns a validation error" do
-      question = described_class.new(number: "two")
+      question.number = "two"
       expect(question).not_to be_valid
       expect(question.errors[:number]).to include(I18n.t("activemodel.errors.models.question/number.attributes.number.not_a_number"))
     end
