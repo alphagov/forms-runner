c694b1241925dec0cf796451804308136ab997f9
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-10-25 12:24:38 +0100

    Change CYA layout for long answers
    When there are long text answers (5000 characyers), the CYO becomes very
    long and the page is hard to navigate.
    
    This commit changes the layout from two thirds to full width if any of
    the answers contain a long-text question type.
    
    Hopefully this makes the page easier to read and navigate, while still
    showing the full text of the answer.

--- spec/views/forms/check_your_answers/show.html.erb_spec.rb
+++ spec/views/forms/check_your_answers/show.html.erb_spec.rb
@@ -2,10 +2,12 @@ require "rails_helper"
 
 describe "forms/check_your_answers/show.html.erb" do
   let(:context) { OpenStruct.new(form_id: 1, form_slug: "slug", mode: "", name: "Form 1") }
+  let(:full_width) { false }
 
   before do
     assign(:current_context, context)
     assign(:form_submit_path, "/")
+    assign(:full_width, full_width)
     render template: "forms/check_your_answers/show"
   end
 
@@ -24,6 +26,18 @@ describe "forms/check_your_answers/show.html.erb" do
 
     it "displays declaration text" do
       expect(rendered).to have_css("p.govuk-body", text: "You should agree to all terms before submitting")
+    end
+  end
+
+  it 'displays two-thirds ' do
+    expect(rendered).to have_css(".govuk-grid-column-two-thirds-from-desktop")
+  end
+
+  context "when full_width not set" do
+    let(:full_width) { true }
+
+    it 'displays full width when set' do
+      expect(rendered).to have_css(".govuk-grid-column-full")
     end
   end
 end
