8aab7ed6947f76fe8dd3fe9a365b3b6fa2af9160
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-10-25 09:33:23 +0100

    Change escape markdown for notify
    When the content for submission emails wrapped answers in code fences to
    prevent markdown interpretation in notify. Long text answers means that
    this won't always work.
    
    This commit changes the escaping strategy to escape special characters
    and normalize whitespace instead.

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -46,7 +46,7 @@ class NotifyService
     form.steps.map { |page|
       [prep_question_title(page.question_text),
        prep_answer_text(page.show_answer)].join
-    }.join("\n\n---\n\n")
+    }.join("\n\n---\n\n").concat("\n")
   end
 
   def prep_question_title(question_text)
@@ -54,7 +54,45 @@ class NotifyService
   end
 
   def prep_answer_text(answer)
-    answer = "[This question was skipped]" if answer.blank?
-    "```\n\n#{answer}\n```\n"
+    return "\\[This question was skipped\\]" if answer.blank?
+
+    escape(answer)
+  end
+
+  def escape(text)
+    text
+      .then { normalize_whitespace _1 }
+      .then { replace_setext_headings _1 }
+      .then { escape_markdown_text _1 }
+  end
+
+  def escape_markdown_text(text)
+    url_regex = URI::DEFAULT_PARSER.make_regexp(%w[http https])
+    a = ""
+    rest = text
+    until rest.empty?
+      head, match, rest = rest.partition(url_regex)
+      a << escape_markdown_characters(head)
+      a << match
+    end
+    a
+  end
+
+  def normalize_whitespace(text)
+    text.strip.gsub(/\r\n?/, "\n").split(/\n\n+/).map(&:strip).join("\n\n")
+  end
+
+  def escape_markdown_characters(text)
+    replaced = { "^" => "", "•" => "" }
+    escaped = %w{! " # ' ` ( ) * + - . [ ] _ \{ | \} ~}.index_with { |c| "\\#{c}" }
+
+    changes = replaced.merge(escaped)
+
+    to_change = Regexp.union(changes.keys)
+    text.gsub(to_change, changes)
+  end
+
+  def replace_setext_headings(text)
+    text.gsub(/^=+$/, '\1&nbsp;')
   end
 end
