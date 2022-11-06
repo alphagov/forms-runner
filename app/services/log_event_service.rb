207d6f5fc73fb30d7b408c4dbbde424e505e414d
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2022-11-06 21:17:56 +0000

    Add log event service

---
+++ app/services/log_event_service.rb
@@ -0,0 +1,35 @@
+class LogEventService
+  def initialize(current_context, step, request, changing_answer, answers)
+    @current_context = current_context
+    @step = step
+    @request = request
+    @changing_answer = changing_answer
+    @answers = answers
+  end
+
+  def log_page_save
+    EventLogger.log_page_event(@current_context, @step, @request, log_event, skipped_question?)
+  end
+
+private
+
+  def log_event
+    page_optional = @step.question.is_optional? ? "optional" : "page"
+
+    if @changing_answer
+      "change_answer_#{page_optional}_save"
+    elsif is_starting_form
+      "first_#{page_optional}_save"
+    else
+      "#{page_optional}_save"
+    end
+  end
+
+  def skipped_question?
+    @step.question.is_optional? ? @answers.each_value.map.none?(&:present?) : nil
+  end
+
+  def is_starting_form
+    @current_context.form_start_page == @step.id
+  end
+end
