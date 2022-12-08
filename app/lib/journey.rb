e7848b47dd14b92784e5fb6e23c33fba6429c883
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-12-08 14:13:28 +0000

    Catch error on loading steps from session
    Catch ActiveModel::UnknownAttributeError when loading steps from session,
    remove the step from the session storage and return nil.
    
    When form fillers are part way through a journey and form creators
    change the question type, the form filler will see an error. The only
    way out of this error is to remove the cookie or wait for the session to
    be invalidated.
    
    This commit catches the error when loading steps and clears the step
    from the storage. The form filler can now continue to fill out the form,
    though will have lost the answer to the original question, which is
    unavoidable.

--- app/lib/journey.rb
+++ app/lib/journey.rb
@@ -14,6 +14,9 @@ private
   def find_existing_step(page_slug)
     step = @step_factory.create_step(page_slug)
     step.load_from_context(@form_context) unless @form_context.get_stored_answer(step).nil?
+  rescue ActiveModel::UnknownAttributeError
+    @form_context.clear_stored_answer(step)
+    nil
   end
 
   def generate_completed_steps
