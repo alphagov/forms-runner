8e62c6fbf25f9708f1c342742039c2e143c2c8eb
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-01-23 16:28:24 +0000

    Add model and view tests
    Co-authored-by: Samuel Culley <SamJamCul@users.noreply.github.com>
    Co-authored-by: Tristram Oaten <o@tman.me>

--- spec/models/question/name_spec.rb
+++ spec/models/question/name_spec.rb
@@ -2,5 +2,312 @@ require "rails_helper"
 
 RSpec.describe Question::Name, type: :model do
   subject(:question) { described_class.new({}, options) }
+
+  let(:options) { { is_optional:, answer_settings: } }
+  let(:answer_settings) { OpenStruct.new({ input_type:, title_needed: }) }
+  let(:input_type) { "full_name" }
+  let(:title_needed) { "false" }
+  let(:is_optional) { false }
+
+  it_behaves_like "a question model"
+
   # TODO: add tests for validations and model methods
+  context "when the name question is in full name format" do
+    context "when the answer is empty" do
+      it "returns invalid with blank full_name field" do
+        expect(question).not_to be_valid
+        expect(question.errors[:full_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.full_name.blank"))
+      end
+
+      it "returns invalid with empty string full_name field" do
+        question.full_name = ""
+        expect(question).not_to be_valid
+        expect(question.errors[:full_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.full_name.blank"))
+      end
+
+      it "returns \"\" for show_answer" do
+        expect(question.show_answer).to eq ""
+      end
+    end
+
+    context "when the answer is valid" do
+      let(:name) { Faker::Name.name }
+
+      before do
+        question.full_name = name
+      end
+
+      it "returns valid" do
+        expect(question).to be_valid
+        expect(question.errors[:full_name]).to be_empty
+      end
+
+      it "returns the full name for show_answer" do
+        expect(question.show_answer).to eq(name)
+      end
+
+      it "returns the labelled individual parts in show_answer_for_email" do
+        expect(question.show_answer_in_email).to eq("Full name: #{name}")
+      end
+    end
+  end
+
+  context "when the name question is in first and last name format" do
+    let(:input_type) { "first_and_last_name" }
+
+    context "when the answer is empty" do
+      it "returns invalid with blank first and last name fields" do
+        expect(question).not_to be_valid
+        expect(question.errors[:first_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.first_name.blank"))
+        expect(question.errors[:last_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.last_name.blank"))
+      end
+
+      it "returns invalid with empty string first and last name fields" do
+        question.first_name = ""
+        question.last_name = ""
+        expect(question).not_to be_valid
+        expect(question.errors[:first_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.first_name.blank"))
+        expect(question.errors[:last_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.last_name.blank"))
+      end
+
+      it "returns \"\" for show_answer" do
+        expect(question.show_answer).to eq ""
+      end
+    end
+
+    context "when the question is optional" do
+      let(:is_optional) { true }
+
+      it "allows the user to skip the question" do
+        question.first_name = ""
+        question.last_name = ""
+        expect(question).to be_valid
+        expect(question.errors[:first_name]).to be_empty
+        expect(question.errors[:last_name]).to be_empty
+      end
+
+      it "returns invalid if the question is partially filled in" do
+        question.first_name = Faker::Name.first_name
+        question.last_name = ""
+        expect(question).not_to be_valid
+        expect(question.errors[:last_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.last_name.blank"))
+      end
+    end
+
+    context "when the answer is valid" do
+      let(:name) { "#{first_name} #{last_name}" }
+      let(:first_name) { Faker::Name.first_name }
+      let(:last_name) { Faker::Name.last_name }
+
+      before do
+        question.first_name = first_name
+        question.last_name = last_name
+      end
+
+      it "returns valid" do
+        expect(question).to be_valid
+        expect(question.errors[:first_name]).to be_empty
+        expect(question.errors[:last_name]).to be_empty
+      end
+
+      it "returns the full name for show_answer" do
+        expect(question.show_answer).to eq(name)
+      end
+
+      it "returns the labelled individual parts in show_answer_for_email" do
+        expect(question.show_answer_in_email).to eq("First name: #{first_name}\n\nLast name: #{last_name}")
+      end
+    end
+  end
+
+  context "when the name question is in first, middle and last name format" do
+    let(:input_type) { "first_middle_and_last_name" }
+
+    context "when the answer is empty" do
+      it "returns invalid with blank first and last name fields" do
+        expect(question).not_to be_valid
+        expect(question.errors[:first_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.first_name.blank"))
+        expect(question.errors[:last_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.last_name.blank"))
+      end
+
+      it "returns invalid with empty string first and last name fields" do
+        question.first_name = ""
+        question.last_name = ""
+        expect(question).not_to be_valid
+        expect(question.errors[:first_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.first_name.blank"))
+        expect(question.errors[:last_name]).to include(I18n.t("activemodel.errors.models.question/name.attributes.last_name.blank"))
+      end
+
+      it "returns \"\" for show_answer" do
+        expect(question.show_answer).to eq ""
+      end
+    end
+
+    context "when the answer is valid" do
+      let(:name) { "#{first_name} #{middle_name} #{last_name}" }
+      let(:first_name) { Faker::Name.first_name }
+      let(:middle_name) { Faker::Name.middle_name }
+      let(:last_name) { Faker::Name.last_name }
+
+      before do
+        question.first_name = first_name
+        question.middle_names = middle_name
+        question.last_name = last_name
+      end
+
+      it "returns valid" do
+        expect(question).to be_valid
+        expect(question.errors[:first_name]).to be_empty
+        expect(question.errors[:middle_names]).to be_empty
+        expect(question.errors[:last_name]).to be_empty
+      end
+
+      it "returns the full name for show_answer" do
+        expect(question.show_answer).to eq(name)
+      end
+
+      it "returns the labelled individual parts in show_answer_for_email" do
+        expect(question.show_answer_in_email).to eq("First name: #{first_name}\n\nMiddle names: #{middle_name}\n\nLast name: #{last_name}")
+      end
+    end
+  end
+
+  context "when title_needed is set to true" do
+    let(:title_needed) { "true" }
+
+    context "when the name question is in full name format" do
+      let(:name) { Faker::Name.name }
+
+      context "when title is blank" do
+        before do
+          question.title = ""
+          question.full_name = name
+        end
+
+        it "allows the user to enter a blank title" do
+          expect(question).to be_valid
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq(name)
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("Full name: #{name}")
+        end
+      end
+
+      context "when title is set" do
+        let(:title) { Faker::Name.prefix }
+
+        before do
+          question.title = title
+          question.full_name = name
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq("#{title} #{name}")
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("Title: #{title}\n\nFull name: #{name}")
+        end
+      end
+    end
+
+    context "when the name question is in first and last name format" do
+      let(:input_type) { "first_and_last_name" }
+      let(:first_name) { Faker::Name.first_name }
+      let(:last_name) { Faker::Name.last_name }
+
+      before do
+        question.title = title
+        question.first_name = first_name
+        question.last_name = last_name
+      end
+
+      context "when title is blank" do
+        let(:title) { nil }
+
+        it "allows the user to enter a blank title" do
+          expect(question).to be_valid
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq("#{first_name} #{last_name}")
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("First name: #{first_name}\n\nLast name: #{last_name}")
+        end
+      end
+
+      context "when title is set" do
+        let(:title) { Faker::Name.prefix }
+
+        before do
+          question.title = title
+          question.first_name = first_name
+          question.last_name = last_name
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq("#{title} #{first_name} #{last_name}")
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("Title: #{title}\n\nFirst name: #{first_name}\n\nLast name: #{last_name}")
+        end
+      end
+    end
+
+    context "when the name question is in first, middle and last name format" do
+      let(:input_type) { "first_and_last_name" }
+      let(:first_name) { Faker::Name.first_name }
+      let(:middle_name) { "#{Faker::Name.middle_name} #{Faker::Name.middle_name}" }
+      let(:last_name) { Faker::Name.last_name }
+
+      before do
+        question.title = title
+        question.first_name = first_name
+        question.middle_names = middle_name
+        question.last_name = last_name
+      end
+
+      context "when title is blank" do
+        let(:title) { nil }
+
+        it "allows the user to enter a blank title" do
+          expect(question).to be_valid
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq("#{first_name} #{middle_name} #{last_name}")
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("First name: #{first_name}\n\nMiddle names: #{middle_name}\n\nLast name: #{last_name}")
+        end
+      end
+
+      context "when title is set" do
+        let(:title) { Faker::Name.prefix }
+
+        before do
+          question.title = title
+          question.first_name = first_name
+          question.middle_names = middle_name
+          question.last_name = last_name
+        end
+
+        it "returns the full name for show_answer" do
+          expect(question.show_answer).to eq("#{title} #{first_name} #{middle_name} #{last_name}")
+        end
+
+        it "returns the labelled individual parts in show_answer_for_email" do
+          expect(question.show_answer_in_email).to eq("Title: #{title}\n\nFirst name: #{first_name}\n\nMiddle names: #{middle_name}\n\nLast name: #{last_name}")
+        end
+      end
+    end
+  end
 end
--- spec/views/question/_name.html.erb_spec.rb
+++ spec/views/question/_name.html.erb_spec.rb
@@ -1,5 +1,94 @@
 require "rails_helper"
 
 describe "question/_name.html.erb" do
-  # TODO add view tests for each variation of the input typeand title fields
+  # TODO: add view tests for each variation of the input typeand title fields
+  let(:page) do
+    Page.new({
+      id: 1,
+      question_text: "What is your name?",
+      question_short_name: nil,
+      hint_text: nil,
+      answer_type: "name",
+      is_optional: false,
+      answer_settings:,
+    })
+  end
+
+  let(:answer_settings) { OpenStruct.new({ input_type:, title_needed: }) }
+  let(:input_type) { "full_name" }
+  let(:title_needed) { "false" }
+
+  let(:question) do
+    QuestionRegister.from_page(page)
+  end
+
+  let(:step) { Step.new(question:, page_id: page.id, form_id: 1, form_slug: "", next_page_slug: 2, page_slug: 1, page_number: 1) }
+
+  let(:form) do
+    GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                  ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+  end
+
+  before do
+    render partial: "question/name", locals: { page: step, form: }
+  end
+
+  context "when the question needs a title" do
+    let(:title_needed) { "true" }
+
+    it "contains the correct autocomplete atrtibute for a title" do
+      expect(rendered).to have_css("input[type='text'][autocomplete='honorific-prefix']")
+    end
+  end
+
+  context "when the question does not need a title" do
+    let(:title_needed) { "false" }
+
+    it "does not contain the title field" do
+      expect(rendered).not_to have_css("input[type='text'][autocomplete='honorific-prefix']")
+    end
+  end
+
+  context "when the question is a full name" do
+    let(:input_type) { "full_name" }
+
+    it "contains the question" do
+      expect(rendered).to have_css("h1", text: page.question_text)
+    end
+
+    it "contains the correct autocomplete attribute for a full name" do
+      expect(rendered).to have_css("input[type='text'][autocomplete='name']")
+    end
+  end
+
+  context "when the question is a name question with first and last name fields" do
+    let(:input_type) { "first_and_last_name" }
+
+    it "contains the question" do
+      expect(rendered).to have_css("h1", text: page.question_text)
+    end
+
+    it "contains the correct autocomplete attributes for a first and last names" do
+      expect(rendered).to have_css("input[type='text'][autocomplete='given-name']")
+      expect(rendered).to have_css("input[type='text'][autocomplete='family-name']")
+    end
+
+    it "does not contain the autocomplete attribute for a middle name" do
+      expect(rendered).not_to have_css("input[type='text'][autocomplete='additional-name']")
+    end
+  end
+
+  context "when the question is a name question with first, middle and last name fields" do
+    let(:input_type) { "first_middle_and_last_name" }
+
+    it "contains the question" do
+      expect(rendered).to have_css("h1", text: page.question_text)
+    end
+
+    it "contains the correct autocomplete attributes for first, middle and last names" do
+      expect(rendered).to have_css("input[type='text'][autocomplete='given-name']")
+      expect(rendered).to have_css("input[type='text'][autocomplete='family-name']")
+      expect(rendered).to have_css("input[type='text'][autocomplete='additional-name']")
+    end
+  end
 end
