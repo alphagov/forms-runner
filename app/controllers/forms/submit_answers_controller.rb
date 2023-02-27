b42918c41ad3fcbc9ac7dc70381c26874abd7d8b
Tristram Oaten <tris@oat.sh>
2023-02-27 16:03:51 +0000

    simple catch of preview submit, probably needs more sentry context?

--- app/controllers/forms/submit_answers_controller.rb
+++ app/controllers/forms/submit_answers_controller.rb
@@ -4,10 +4,15 @@ module Forms
       unless preview?
         EventLogger.log_form_event(current_context, request, "submission")
       end
-
-      FormSubmissionService.call(form: current_context,
+      if form.email.nil?
+        Sentry.capture_message("preview submitted, caught")
+        render "errors/submission_error", status: :internal_server_error
+      else
+        FormSubmissionService.call(form: current_context,
                                  reference: params[:notify_reference],
                                  preview_mode: preview?).submit_form_to_processing_team
+
+      end
 
       current_context.clear
       redirect_to :form_submitted
