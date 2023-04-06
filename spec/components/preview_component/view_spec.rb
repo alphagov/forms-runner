354b925c229c9fa2b3d2915785f39232a4fcb2ae
Thomas Iles <tom.iles@digital.cabinet-office.gov.uk>
2023-04-06 09:58:08 +0100

    Change tests to use translations
    Change the preview header tests to compare directly against the translations rather than strings.
    
    Co-authored-by: David Biddle <david.biddle@digital.cabinet-office.gov.uk>

--- spec/components/preview_component/view_spec.rb
+++ spec/components/preview_component/view_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe PreviewComponent::View, type: :component do
     mode = Mode.new('preview-draft')
     render_inline(described_class.new(mode: mode))
     expect(page).to have_selector(".govuk-phase-banner")
-    expect(page).to have_content("Draft preview")
-    expect(page).to have_content("You're previewing the draft version of this form")
+    expect(page).to have_content(I18n.t("mode.phase_banner_tag_preview-draft"))
+    expect(page).to have_content(I18n.t("mode.phase_banner_text_preview-draft"))
   end
 
   it "shows in preview_live" do
     mode = Mode.new('preview-live')
     render_inline(described_class.new(mode: mode))
     expect(page).to have_selector(".govuk-phase-banner")
-    expect(page).to have_content("Live preview")
-    expect(page).to have_content("You're previewing the live version of this form")
+    expect(page).to have_content(I18n.t("mode.phase_banner_tag_preview-live"))
+    expect(page).to have_content(I18n.t("mode.phase_banner_text_preview-live"))
   end
 
   it "does not show in live" do
