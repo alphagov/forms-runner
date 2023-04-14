67ab85949844880a013d2343e219047ab7069446
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-04-14 11:35:49 +0100

    Add MainComponent
    Add a new component which will create the <main> tag, setting the
    correct classes to display the watermarks in preview.
    
    The sizes have been set with Agz

---
+++ spec/components/main_component/view_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe MainComponent::View, type: :component do
+  it "adds class for mode" do
+    mode = 'preview-draft'
+    render_inline(described_class.new(mode:))
+    expect(page).to have_selector(".main--preview-draft")
+  end
+
+  it "does not add class for empty mode" do
+    mode = ''
+    render_inline(described_class.new(mode:))
+    expect(page).not_to have_selector(".main--")
+  end
+
+  it "adds question class if is_question is true" do
+    mode = ''
+    render_inline(described_class.new(mode:, is_question: true))
+    expect(page).to have_selector(".main--question")
+  end
+
+  it "does not add question class if is_question is not true" do
+    mode = ''
+    render_inline(described_class.new(mode:))
+    expect(page).not_to have_selector(".main--question")
+  end
+end
