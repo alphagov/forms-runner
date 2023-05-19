43a03306d766c7a4920a851d04a1d75f88c9c5e9
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-05-19 14:44:59 +0100

    Switch Context.what_happens_next_text to Context.form.what_happens_next_text

--- app/lib/context.rb
+++ app/lib/context.rb
@@ -1,6 +1,6 @@
 class Context
   attr_accessor :form
-  attr_reader :form_name, :form_start_page, :what_happens_next_text, :support_details, :declaration_text
+  attr_reader :form_name, :form_start_page, :support_details, :declaration_text
 
   def initialize(form:, store:)
     @form = form
@@ -11,7 +11,6 @@ class Context
     @completed_steps = @journey.completed_steps
     @form_name = form.name
     @form_start_page = form.start_page
-    @what_happens_next_text = form.what_happens_next_text
     @declaration_text = form.declaration_text
     @support_details = OpenStruct.new({
       email: form.support_email,
