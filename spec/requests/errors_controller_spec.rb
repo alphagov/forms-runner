4a0049d7f46f1b5d7d72a56f74f0750112437168
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-05-20 11:59:26 +0100

    Use rails url helpers instead of hardcoded strings

--- spec/requests/errors_controller_spec.rb
+++ spec/requests/errors_controller_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe ErrorsController, type: :request do
   describe "Page not found" do
     it "returns http code 404" do
-      get "/404"
+      get error_404_path
       expect(response).to have_http_status(:not_found)
     end
   end
 
   describe "root path" do
     it "returns http code 404" do
-      get "/"
+      get root_path
       expect(response).to have_http_status(:not_found)
     end
   end
@@ -29,7 +29,7 @@ RSpec.describe ErrorsController, type: :request do
 
   describe "Internal server error" do
     it "returns http code 500" do
-      get "/500"
+      get error_500_path
       expect(response).to have_http_status(:internal_server_error)
     end
   end
@@ -37,7 +37,7 @@ RSpec.describe ErrorsController, type: :request do
   describe "Service unavailable page" do
     it "returns http code 503" do
       stub_const "ENV", ENV.to_h.merge("SERVICE_UNAVAILABLE" => "true")
-      get "/"
+      get root_path
       expect(response).to have_http_status(:service_unavailable)
     end
   end
