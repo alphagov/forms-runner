806d6494944032296b24bd63d07dc0ff5f6ec885
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-06-23 13:33:20 +0100

    Add bypass maintenance pages if user ip within bypass ip range

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController, type: :request do
+  subject(:application_controller) { described_class.new }
+
   describe "Accessibility statement" do
     it "returns http code 200" do
       get accessibility_statement_path
@@ -16,10 +18,16 @@ RSpec.describe ApplicationController, type: :request do
   end
 
   context "when the service is in maintenance mode" do
+    let(:bypass_ips) { " " }
+    let(:expect_response_to_redirect) { true }
+    let(:user_ip) { "192.0.0.2" }
+
     before do
       allow(Settings.maintenance_mode).to receive(:enabled).and_return(true)
-      get cookies_path
-      follow_redirect!
+      allow(Settings.maintenance_mode).to receive(:bypass_ips).and_return(bypass_ips)
+
+      get cookies_path, headers: { "HTTP_X_FORWARDED_FOR": user_ip }
+      follow_redirect! if expect_response_to_redirect
     end
 
     it "returns http code 200" do
@@ -28,6 +36,49 @@ RSpec.describe ApplicationController, type: :request do
 
     it "renders the maintenance page" do
       expect(response).to render_template("errors/maintenance")
+    end
+
+    context "when bypass ip range does not cover the user's ip" do
+      let(:bypass_ips) { "192.0.0.0/32, 123.123.123.123/32" }
+
+      it "returns http code 200" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the maintenance page" do
+        expect(response).to render_template("errors/maintenance")
+      end
+    end
+
+    context "when the  bypass ip range does include the user's ip" do
+      let(:bypass_ips) { "192.0.0.0/29, 123.123.123.123/32" }
+      let(:expect_response_to_redirect) { false }
+
+      it "returns http code 200" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the cookie page" do
+        expect(response).to render_template("application/cookies")
+      end
+    end
+  end
+
+  describe "#user_ip" do
+    [
+      ["", nil],
+      ["127.0.0.1", "127.0.0.1"],
+      ["127.0.0.1, 192.168.0.128", "127.0.0.1"],
+      ["185.93.3.65, 15.158.44.215, 10.0.1.94", "185.93.3.65"],
+      ["    185.93.3.65, 15.158.44.215, 10.0.1.94", nil],
+      ["invalid value, 192.168.0.128", nil],
+      ["192.168.0.128.123.2981", nil],
+      ["2001:db8::, 2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF, ::1234:5678", "2001:db8::"],
+      [",,,,,,,,,,,,,,,,,,,,,,,,", nil],
+    ].each do |value, expected|
+      it "returns #{expected.inspect} when given forwarded_for #{value.inspect}" do
+        expect(application_controller.user_ip(value)).to eq(expected)
+      end
     end
   end
 end
