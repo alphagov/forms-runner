f59eea53877b110699a2c9bcd47f6e2be28aca09
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-06-23 14:29:47 +0100

    Add environment tags

--- app/components/form_header_component/view.rb
+++ app/components/form_header_component/view.rb
@@ -1,9 +1,10 @@
 module FormHeaderComponent
   class View < ViewComponent::Base
-    def initialize(current_context:, mode:, service_url_overide: :not_set)
+    def initialize(current_context:, mode:, service_url_overide: :not_set, hosting_environment: HostingEnvironment)
       @current_context = current_context
       @mode = mode
       @service_url_overide = service_url_overide
+      @hosting_environment = hosting_environment
       super
     end
 
@@ -12,13 +13,38 @@ module FormHeaderComponent
         govuk_header(service_name: @current_context.form.name,
                      homepage_url: "https://www.gov.uk/",
                      service_url:,
-                     classes: "app-header--#{@mode}")
+                     classes: ["app-header", "app-header--#{@mode}"]) do |header|
+          header.with_product_name(name: service_name_with_tag)
+        end
       else
-        govuk_header(homepage_url: "https://www.gov.uk/")
+        govuk_header(homepage_url: "https://www.gov.uk/", classes: ["app-header", "app-header--#{@mode}"]) do |header|
+          header.with_product_name(name: service_name_with_tag)
+        end
       end
     end
 
   private
+
+    def service_name_with_tag
+      govuk_tag(colour: colour_for_environment, text: environment_name).html_safe unless environment_name == "production"
+    end
+
+    def environment_name
+      @hosting_environment.friendly_environment_name
+    end
+
+    def colour_for_environment
+      case environment_name
+      when "local"
+        "pink"
+      when "development"
+        "green"
+      when "staging"
+        "yellow"
+      else
+        "blue"
+      end
+    end
 
     def service_url
       if @service_url_overide == :not_set
