acbf34aac11e9136116f274681dfd8b8bd83f95b
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-07-14 16:39:36 +0100

    Convert number question partial to view component
    As part of this refactor it will mean that this answer type
    can now be easily ported to other apps because it doesn't
    rely on specific application specific code. There is only
    one dependency for it which is the govuk form builder gem.

---
+++ spec/components/question/number_component/number_component_preview.rb
@@ -0,0 +1,23 @@
+class Question::NumberComponent::NumberComponentPreview < ViewComponent::Preview
+  def number_field
+    question = OpenStruct.new(number: "7",
+                              answer_type: "number",
+                              question_text: "Number of days in a week",
+                              answer_settings: nil)
+    form_builder = GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                                 ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+
+    render(Question::NumberComponent::View.new(form_builder:, question:, extra_question_text_suffix: ""))
+  end
+
+  def number_field_with_hint
+    question = OpenStruct.new(number: "7",
+                              answer_type: "number",
+                              question_text: "Number of days in a week",
+                              answer_settings: nil)
+    form_builder = GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                                 ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+
+    render(Question::NumberComponent::View.new(form_builder:, question:, extra_question_text_suffix: ""))
+  end
+end
