366906e9d3ad43c9ed7ee58f002b1c67f3386513
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-07-18 12:28:43 +0100

    Convert phone number question partial to view component
    As part of this refactor it will mean that this answer type can now be
    easily ported to other apps because it doesn't rely on specific application
    specific code. There is only one dependency for it which is the govuk form
    builder gem.

---
+++ spec/components/question/phone_number_component/phone_number_component_preview.rb
@@ -0,0 +1,24 @@
+class Question::PhoneNumberComponent::PhoneNumberComponentPreview < ViewComponent::Preview
+  def phone_number_field
+    question = OpenStruct.new(phone_number: "0207 555 4444",
+                              answer_type: "phone_number",
+                              question_text: "What is your home phone number?",
+                              answer_settings: nil)
+    form_builder = GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                                 ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+
+    render(Question::PhoneNumberComponent::View.new(form_builder:, question:, extra_question_text_suffix: ""))
+  end
+
+  def phone_number_field_with_hint
+    question = OpenStruct.new(phone_number: "0207 555 4444",
+                              answer_type: "phone_number",
+                              question_text: "What is your home phone number?",
+                              hint_text: "Do not include international dialing code",
+                              answer_settings: nil)
+    form_builder = GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                                 ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+
+    render(Question::PhoneNumberComponent::View.new(form_builder:, question:, extra_question_text_suffix: ""))
+  end
+end
---
+++ spec/components/question/phone_number_component/view_spec.rb
@@ -0,0 +1,68 @@
+require "rails_helper"
+
+RSpec.describe Question::PhoneNumberComponent::View, type: :component do
+  let(:question_page) { build :page, answer_type: "phone_number" }
+  let(:answer_text) { nil }
+  let(:question) { DataStruct.new(phone_number: answer_text, question_text: question_page.question_text, hint_text: question_page.hint_text, answer_settings: nil) }
+  let(:extra_question_text_suffix) { nil }
+  let(:form_builder) do
+    GOVUKDesignSystemFormBuilder::FormBuilder.new(:form, question,
+                                                  ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil), {})
+  end
+
+  before do
+    render_inline(described_class.new(form_builder:, question:, extra_question_text_suffix:))
+  end
+
+  describe "when component is phone number field" do
+    it "renders the question text as a heading" do
+      expect(page.find("h1")).to have_text(question.question_text)
+    end
+
+    it "renders a text input field" do
+      expect(page).to have_css("input[type='text'][name='form[phone_number]']")
+    end
+
+    it "renders at one half the width of the parent container" do
+      expect(page.native.to_html).to include('class="govuk-input govuk-!-width-one-half"')
+    end
+
+    it "renders the text input field with autocomplete attribute" do
+      expect(page).to have_css("input[type='text'][name='form[phone_number]'][autocomplete='tel']")
+    end
+
+    context "when the user has provided an answer" do
+      let(:answer_text) { 8 }
+
+      it "sets the field value" do
+        expect(page.find("input[type='text'][name='form[phone_number]']").value).to eq answer_text.to_s
+      end
+    end
+
+    context "when the question has hint text" do
+      let(:question_page) { build :page, :with_hints, answer_type: "phone_number" }
+
+      it "outputs the hint text" do
+        expect(page.find(".govuk-hint")).to have_text(question.hint_text)
+      end
+    end
+
+    context "when there is extra suffix to be added to heading" do
+      let(:extra_question_text_suffix) { "Some extra text to add to the question text" }
+
+      it "renders the question text and extra suffix as a heading" do
+        expect(page.find("h1")).to have_text("#{question.question_text} #{extra_question_text_suffix}")
+      end
+    end
+
+    context "with unsafe question text" do
+      let(:question_page) { build :page, answer_type: "number", question_text: "What is your name? <script>alert(\"Hi\")</script>" }
+      let(:extra_question_text_suffix) { "<span>Some trusted html</span>" }
+
+      it "returns the escaped title with the optional suffix" do
+        expected_output = "What is your name? &lt;script&gt;alert(\"Hi\")&lt;/script&gt; <span>Some trusted html</span>"
+        expect(page.find("h1 .govuk-label").native.inner_html).to eq(expected_output)
+      end
+    end
+  end
+end
