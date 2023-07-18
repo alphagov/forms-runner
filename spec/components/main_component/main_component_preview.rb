651e8d0468889e5867f3c1d8422fb02a0a6d8d63
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-07-18 15:10:35 +0100

    Fix issue with nested main components

--- spec/components/main_component/main_component_preview.rb
+++ spec/components/main_component/main_component_preview.rb
@@ -1,28 +1,28 @@
 class MainComponent::MainComponentPreview < ViewComponent::Preview
   def deafult
-    render(MainComponent::View.new(mode: nil))
+    render(MainComponent::View.new(mode: nil, is_component_preview: true))
   end
 
   def draft_preview
-    render(MainComponent::View.new(mode: "preview-draft")) do
+    render(MainComponent::View.new(mode: "preview-draft", is_component_preview: true)) do
       Array.new(10) { content_tag(:p, "This is the draft preview example content.") }.join.html_safe
     end
   end
 
   def live_preview
-    render(MainComponent::View.new(mode: "preview-live")) do
+    render(MainComponent::View.new(mode: "preview-live", is_component_preview: true)) do
       Array.new(10) { content_tag(:p, "This is the live preview example content.") }.join.html_safe
     end
   end
 
   def draft_preview_question
-    render(MainComponent::View.new(mode: "preview-draft", is_question: true)) do
+    render(MainComponent::View.new(mode: "preview-draft", is_question: true, is_component_preview: true)) do
       Array.new(10) { content_tag(:p, "This is the draft preview example content for question pages.") }.join.html_safe
     end
   end
 
   def live_preview_question
-    render(MainComponent::View.new(mode: "preview-live", is_question: true)) do
+    render(MainComponent::View.new(mode: "preview-live", is_question: true, is_component_preview: true)) do
       Array.new(10) { content_tag(:p, "This is the live preview example content for question pages.") }.join.html_safe
     end
   end
