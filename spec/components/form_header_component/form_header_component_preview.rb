b52dca513a27d7e7c16605b8c509a2bfade1bfe9
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-07-18 15:08:55 +0100

    Fix header preview

--- spec/components/form_header_component/form_header_component_preview.rb
+++ spec/components/form_header_component/form_header_component_preview.rb
@@ -1,19 +1,19 @@
 class FormHeaderComponent::FormHeaderComponentPreview < ViewComponent::Preview
   def default
     mode = Mode.new
-    current_context = OpenStruct.new(form: 1, form_name: "test", form_slug: "test")
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
     render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
   end
 
   def preview_draft
     mode = Mode.new("preview-draft")
-    current_context = OpenStruct.new(form: 1, form_name: "test", form_slug: "test")
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
     render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
   end
 
   def preview_live
     mode = Mode.new("preview-live")
-    current_context = OpenStruct.new(form: 1, form_name: "test", form_slug: "test")
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
     render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
   end
 end
