271cb8d00d561bf65bd54cc58320e6ae0f427f59
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-08-09 13:21:02 +0100

    Remove references to PaaS
    This includes references to VCAP services which are all related to PaaS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@ require_relative "../../app/lib/redis_config"
 redis_url = RedisConfig.redis_url
 
 if redis_url.blank?
-  throw StandardError.new "You must configure a session store using REDIS_URL or VCAP_SERVICES" unless Rails.configuration.try(:unsafe_session_storage)
+  throw StandardError.new "You must configure a session store using REDIS_URL" unless Rails.configuration.try(:unsafe_session_storage)
   Rails.logger.debug "WARNING: Using cookies as session store, insecure\n"
 else
   Rails.application.config.session_store :redis_session_store,
--- spec/lib/redis_config_spec.rb
+++ spec/lib/redis_config_spec.rb
@@ -3,20 +3,10 @@ require "spec_helper"
 require_relative "../../app/lib/redis_config"
 
 RSpec.describe RedisConfig do
-  let(:vcap_env) { nil }
   let(:redis_url_env) { nil }
 
   before do
-    allow(ENV).to receive(:fetch).with("VCAP_SERVICES", nil).and_return(vcap_env)
     allow(ENV).to receive(:fetch).with("REDIS_URL", nil).and_return(redis_url_env)
-  end
-
-  context "when VCAP_SERVICES is set" do
-    let(:vcap_env) { File.read(File.join("spec", "fixtures", "vcap_example.json")) }
-
-    it "extracts the uri from JSON" do
-      expect(described_class.redis_url).to eq("rediss://:password@redis.example.org:6379")
-    end
   end
 
   context "when REDIS_URL is set" do
@@ -24,15 +14,6 @@ RSpec.describe RedisConfig do
 
     it "equals REDIS_URL" do
       expect(described_class.redis_url).to eq("redis_url_value")
-    end
-  end
-
-  context "when VCAP_SERVICES and REDIS_URL are set" do
-    let(:vcap_env) { File.read(File.join("spec", "fixtures", "vcap_example.json")) }
-    let(:redis_url_env) { "redis_url_value" }
-
-    it "uses the VCAP_SERVICES url" do
-      expect(described_class.redis_url).to eq("rediss://:password@redis.example.org:6379")
     end
   end
 
