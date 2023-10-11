e0b4e639555cdb18c698ca4c232281d9b245cb2e
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2023-10-11 10:52:54 +0100

    Add .log_form_start to CloudWatch service

--- app/services/cloud_watch_service.rb
+++ app/services/cloud_watch_service.rb
@@ -20,6 +20,27 @@ class CloudWatchService
     )
   end
 
+  def self.log_form_start(form_id:)
+    region = "eu-west-2"
+    cloudwatch_client = Aws::CloudWatch::Client.new(region:)
+    cloudwatch_client.put_metric_data(
+      namespace: metric_namespace,
+      metric_data: [
+        {
+          metric_name: "started",
+          dimensions: [
+            {
+              name: "form_id",
+              value: form_id.to_s,
+            },
+          ],
+          value: 1,
+          unit: "Count",
+        },
+      ],
+    )
+  end
+
   def self.metric_namespace
     "forms/#{Settings.forms_env}".downcase
   end
