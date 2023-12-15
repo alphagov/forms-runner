e5b79022a5dfd9a5653e4a26f50b9f9058107c04
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-12-15 14:58:50 +0000

    Change live and draft colours
    Changes the colour of 'live' tags to turquoise, and 'draft' tags to
    yellow. Also adds the tag selector to the tests, so we can check the
    right colours are appearing.

--- spec/components/preview_component/view_spec.rb
+++ spec/components/preview_component/view_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PreviewComponent::View, type: :component do
     mode = Mode.new("preview-draft")
     render_inline(described_class.new(mode:, question_edit_link: "https://gov.uk"))
     expect(page).to have_selector(".govuk-phase-banner")
-    expect(page).to have_content(I18n.t("mode.phase_banner_tag_preview-draft"))
+    expect(page).to have_selector(".govuk-tag--yellow", text: I18n.t("mode.phase_banner_tag_preview-draft"))
     expect(page).to have_content(I18n.t("mode.phase_banner_text_preview-draft"))
     expect(page).to have_link(I18n.t("mode.phase_banner_text_preview-draft_edit_link", href: "https://gov.uk"))
   end
@@ -14,7 +14,7 @@ RSpec.describe PreviewComponent::View, type: :component do
     mode = Mode.new("preview-live")
     render_inline(described_class.new(mode:))
     expect(page).to have_selector(".govuk-phase-banner")
-    expect(page).to have_content(I18n.t("mode.phase_banner_tag_preview-live"))
+    expect(page).to have_selector(".govuk-tag--turquoise", text: I18n.t("mode.phase_banner_tag_preview-live"))
     expect(page).to have_content(I18n.t("mode.phase_banner_text_preview-live"))
   end
 
