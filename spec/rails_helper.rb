b29f7b44041f66ce7300d06d735c3d9209bd5006
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-01-04 07:40:08 +0000

    Shift spec support file loading to later in rails_helper.rb
    The loading of spec support files was moved from the start to a later part of the rails_helper.rb which is the default position for it when first install rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,8 +10,6 @@ require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
-
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -27,8 +25,8 @@ require "rspec/rails"
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
