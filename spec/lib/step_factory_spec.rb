a643c67375633df40a8dab21fc02c686313aa99f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-01-12 14:06:50 +0200

    Refactor expectation to result in more useful error message

--- spec/lib/step_factory_spec.rb
+++ spec/lib/step_factory_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe StepFactory do
   describe StepFactory::PAGE_SLUG_REGEX do
     it "matches valid page_id values" do
       %w[1 123 0123456789 check_your_answers].each do |string|
-        expect(described_class.match(string)).to be_truthy
+        expect(described_class).to match string
       end
     end
 
     it "does not match invalid page_id values" do
       %w[no ten inspect_your_answers /secret/login.php].each do |string|
-        expect(described_class.match(string)).to be_falsy
+        expect(described_class).not_to match string
       end
     end
   end
