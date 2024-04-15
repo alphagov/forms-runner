acec21e6c15959d474b093ac71bf121fd0f702ee
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-04-15 10:20:36 +0100

    Remove preview watermark logic
    Our accessibility audit raised the watermark as an issue since it:
    - sits underneath the text, which can cause the text contrast to become
    too low
    - repeats throughout the main page background, which some users found
    distracting.
    
    This PR removes the watermark to resolve this. Since we initially added
    the watermark, we've added a notification banner telling people this is
    a preview, so we think the watermark is probably unnecessary.

--- app/components/main_component/view.rb
+++ app/components/main_component/view.rb
@@ -2,27 +2,15 @@
 
 module MainComponent
   class View < ViewComponent::Base
-    def initialize(mode:, is_question: false, is_component_preview: false)
+    def initialize(is_component_preview: false)
       super
-      @mode = mode
-      @is_question = is_question
       @is_component_preview = is_component_preview
     end
 
     def call
-      tag.main(class: "govuk-main-wrapper #{mode_class} #{is_question_class}", id: @is_component_preview ? nil : "main-content", role: "main") do
+      tag.main(class: "govuk-main-wrapper", id: @is_component_preview ? nil : "main-content", role: "main") do
         content
       end
-    end
-
-  private
-
-    def mode_class
-      "main--#{@mode}" if @mode.present?
-    end
-
-    def is_question_class
-      "main--question" if @is_question
     end
   end
 end
--- spec/components/main_component/view_spec.rb
+++ spec/components/main_component/view_spec.rb
@@ -1,27 +1,13 @@
 require "rails_helper"
 
 RSpec.describe MainComponent::View, type: :component do
-  it "adds class for mode" do
-    mode = "preview-draft"
-    render_inline(described_class.new(mode:))
-    expect(page).to have_selector(".main--preview-draft")
+  it "returns the main content ID" do
+    render_inline(described_class.new)
+    expect(page).to have_selector("#main-content")
   end
 
-  it "does not add class for empty mode" do
-    mode = ""
-    render_inline(described_class.new(mode:))
-    expect(page).not_to have_selector(".main--")
-  end
-
-  it "adds question class if is_question is true" do
-    mode = ""
-    render_inline(described_class.new(mode:, is_question: true))
-    expect(page).to have_selector(".main--question")
-  end
-
-  it "does not add question class if is_question is not true" do
-    mode = ""
-    render_inline(described_class.new(mode:))
-    expect(page).not_to have_selector(".main--question")
+  it "does not return the main content ID" do
+    render_inline(described_class.new(is_component_preview: true))
+    expect(page).not_to have_selector("#main-content")
   end
 end
