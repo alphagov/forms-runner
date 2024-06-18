429d08305c4ccef4552d0aa7a47f1f60c4fb6bd9
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-06-18 14:18:51 +0100

    Add google analytics behind flag
    Adds a subset of the analytics code from the product pages repo to this
    one and runs it when the settings flag is enabled.
    
    We'll need more of the code from the product pages later to deal with
    the cookie consent, but that's being handled in a separate card so I'm
    eeping it out of this PR for tidiness.

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,3 +1,8 @@
 import { initAll } from 'govuk-frontend'
+import { installAnalyticsScript } from '../javascript/google-tag'
+
+if (document.body.dataset.googleAnalyticsEnabled === 'true') {
+  installAnalyticsScript(window)
+}
 
 initAll()
