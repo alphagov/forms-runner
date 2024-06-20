9c1ce4494cc227285bd0e0d450790da98934c44a
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-06-20 10:33:58 +0100

    Add cookie banner
    Adds the cookie banner HTML and its associated JS component from the
    product pages repo.

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,8 +1,47 @@
 import { initAll } from 'govuk-frontend'
-import { installAnalyticsScript } from '../javascript/utils/google-analytics'
+import {
+  loadConsentStatus,
+  saveConsentStatus,
+  CONSENT_STATUS
+} from '../javascript/utils/cookie-consent'
 
-if (document.body.dataset.googleAnalyticsEnabled === 'true') {
+import {
+  installAnalyticsScript,
+  deleteGoogleAnalyticsCookies,
+  setDefaultConsent,
+  updateCookieConsent
+} from '../javascript/utils/google-analytics'
+import { CookieBanner } from '../javascript/cookie-banner'
+
+const analyticsConsentStatus = loadConsentStatus()
+
+setDefaultConsent(analyticsConsentStatus === CONSENT_STATUS.GRANTED)
+
+if (
+  document.body.dataset.googleAnalyticsEnabled === 'true' &&
+  analyticsConsentStatus === CONSENT_STATUS.GRANTED
+) {
   installAnalyticsScript(window)
 }
 
+// Initialise cookie banner
+const $banners = document.querySelectorAll('[data-module="cookie-banner"]')
+$banners.forEach(function ($banner) {
+  new CookieBanner($banner).init({
+    showBanner: analyticsConsentStatus === CONSENT_STATUS.UNKNOWN,
+    onSubmit: handleUpdateConsent
+  })
+})
+
+function handleUpdateConsent (consentedToAnalyticsCookies) {
+  saveConsentStatus(consentedToAnalyticsCookies)
+
+  updateCookieConsent(consentedToAnalyticsCookies)
+
+  if (consentedToAnalyticsCookies === false) {
+    deleteGoogleAnalyticsCookies()
+  } else {
+    installAnalyticsScript(window)
+  }
+}
 initAll()
--- spec/features/analytics/analytics_spec.rb
+++ spec/features/analytics/analytics_spec.rb
@@ -12,14 +12,48 @@ feature "Google analytics", type: :feature do
       visit root_path
       expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
     end
+
+    it "contains the cookie banner" do
+      visit root_path
+      expect(page).to have_css(".govuk-cookie-banner", visible: :visible)
+    end
   end
 
   context "when the analytics setting is enabled" do
     let(:analytics_enabled) { true }
 
-    it "loads in Google Analytics" do
+    context "when the user has not set a cookie consent status" do
+      it "does not load in Google Analytics" do
+        visit root_path
+        expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+      end
+    end
+
+    context "when the user has rejected cookies" do
+      before do
+        visit root_path
+        click_button "Reject analytics cookies"
+      end
+
+      it "does not load in Google Analytics" do
+        expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+      end
+    end
+
+    context "when the user has accepted cookies" do
+      before do
+        visit root_path
+        click_button "Accept analytics cookies"
+      end
+
+      it "loads in Google Analytics" do
+        expect(page).to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+      end
+    end
+
+    it "contains the cookie banner" do
       visit root_path
-      expect(page).to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+      expect(page).to have_css(".govuk-cookie-banner", visible: :visible)
     end
   end
 end
