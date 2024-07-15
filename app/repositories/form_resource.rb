0c632b55d9ef051f7be302aa9fc7df1e67092c2b
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-07-15 09:29:07 +0100

    Move PageResource to top scope in FormResource form repository
    Co-authored-by: Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
    Co-authored-by: Tom Iles <tom.iles@digital.cabinet-office.gov.uk>

--- app/repositories/form_resource.rb
+++ app/repositories/form_resource.rb
@@ -1,16 +1,16 @@
+# needed to convert pages using ActiveResource
+class PageResource
+  def self.new(attributes, _persisted)
+    Page.from_json(attributes)
+  end
+end
+
 class FormResource < ActiveResource::Base
   self.site = Settings.forms_api.base_url
   self.element_name = "form"
   self.prefix = "/api/v1/"
   self.include_format_in_path = false
   headers["X-API-Token"] = Settings.forms_api.auth_key
-
-  # needed to convert pages using ActiveResource
-  class PageResource
-    def self.new(attributes, persisted, &block)
-      Page.from_json(attributes)
-    end
-  end
 
   include FormRepository
 
