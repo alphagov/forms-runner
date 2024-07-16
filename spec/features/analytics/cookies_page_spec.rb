cb60ceb5f5ec128640f1e26d0c7a41b9a0f622fa
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-07-16 16:52:38 +0100

    Use capybara-friendly scenario syntax in feature test

--- spec/features/analytics/cookies_page_spec.rb
+++ spec/features/analytics/cookies_page_spec.rb
@@ -1,141 +1,133 @@
 require "rails_helper"
 
-feature "Cookies page", type: :feature do
-  before do
-    allow(Settings).to receive(:analytics_enabled).and_return(analytics_enabled)
+feature "Cookies page" do
+  scenario "with analytics turned off" do
+    when_analytics_is_not_enabled
+    when_i_visit_the_cookies_page
+    it_does_not_load_google_analytics
+    it_does_not_display_the_cookie_banner
+    it_does_not_display_the_cookie_consent_form
+    it_does_not_display_the_non_js_message
+  end
+
+  scenario "when the user has not set a cookie consent status" do
+    when_analytics_is_enabled
+    when_i_visit_the_cookies_page
+    it_does_not_load_google_analytics
+    it_does_not_display_the_cookie_banner
+    it_displays_the_cookie_consent_form
+    it_does_not_display_the_success_message
+    it_does_not_display_the_non_js_message
+  end
+
+  scenario "when the user rejects cookies" do
+    when_analytics_is_enabled
+    when_i_visit_the_cookies_page
+    then_i_reject_analytics_cookies
+    it_does_not_load_google_analytics
+    it_does_not_display_the_cookie_banner
+    it_displays_the_cookie_consent_form
+    it_checks_the_no_radio_button_by_default
+    it_displays_the_success_message
+    it_does_not_display_the_non_js_message
+  end
+
+  scenario "when the user accepts cookies" do
+    when_analytics_is_enabled
+    when_i_visit_the_cookies_page
+    then_i_accept_analytics_cookies
+    it_loads_google_analytics
+    it_does_not_display_the_cookie_banner
+    it_displays_the_cookie_consent_form
+    it_checks_the_yes_radio_button_by_default
+    it_displays_the_success_message
+    it_does_not_display_the_non_js_message
+  end
+
+  scenario "without javascript enabled", driver: :rack_test do
+    when_analytics_is_enabled
+    when_i_visit_the_cookies_page
+    it_does_not_load_google_analytics
+    it_does_not_display_the_cookie_banner
+    it_does_not_display_the_cookie_consent_form
+    it_does_not_display_the_success_message
+    it_displays_the_non_js_message
+  end
+
+private
+
+  def when_analytics_is_enabled
+    allow(Settings).to receive(:analytics_enabled).and_return(true)
+  end
+
+  def when_analytics_is_not_enabled
+    allow(Settings).to receive(:analytics_enabled).and_return(false)
+  end
+
+  def when_i_visit_the_cookies_page
     visit cookies_path
   end
 
-  context "when the analytics setting is not enabled" do
-    let(:analytics_enabled) { false }
-
-    it "does not load in Google Analytics" do
-      expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+  def then_i_accept_analytics_cookies
+    within_fieldset "Do you want to accept analytics cookies?" do
+      choose "Yes"
     end
 
-    it "does not display the cookie banner" do
-      expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
-    end
-
-    it "does not display the non-JS message" do
-      expect(page).not_to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
-    end
+    click_button "Save cookie settings"
   end
 
-  context "when the analytics setting is enabled" do
-    let(:analytics_enabled) { true }
-
-    context "when the user has not set a cookie consent status" do
-      it "does not load in Google Analytics" do
-        expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
-      end
-
-      it "does not display the cookie banner" do
-        expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
-      end
-
-      it "displays the cookie consent form" do
-        expect(page).to have_css("legend", text: "Do you want to accept analytics cookies?", visible: :visible)
-        expect(page).to have_field("Yes", type: :radio, visible: :hidden)
-        expect(page).to have_field("No", type: :radio, visible: :hidden)
-        expect(page).to have_button("Save cookie settings", visible: :visible)
-      end
-
-      it "does not display the success message" do
-        expect(page).not_to have_text("You’ve set your cookie preferences.")
-      end
-
-      it "does not display the non-JS message" do
-        expect(page).not_to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
-      end
+  def then_i_reject_analytics_cookies
+    within_fieldset "Do you want to accept analytics cookies?" do
+      choose "No"
     end
 
-    context "when the user has rejected cookies" do
-      before do
-        within_fieldset "Do you want to accept analytics cookies?" do
-          choose "No"
-        end
+    click_button "Save cookie settings"
+  end
 
-        click_button "Save cookie settings"
-      end
+  def it_loads_google_analytics
+    expect(page).to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+  end
 
-      it "does not load in Google Analytics" do
-        expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
-      end
+  def it_does_not_load_google_analytics
+    expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
+  end
 
-      it "does not display the cookie banner" do
-        expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
-      end
+  def it_does_not_display_the_cookie_banner
+    expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
+  end
 
-      it "displays the cookie consent form" do
-        expect(page).to have_css("legend", text: "Do you want to accept analytics cookies?", visible: :visible)
-        expect(page).to have_field("Yes", visible: :hidden)
-        expect(page).to have_checked_field("No", visible: :hidden)
-        expect(page).to have_button("Save cookie settings")
-      end
+  def it_displays_the_cookie_consent_form
+    expect(page).to have_css("legend", text: "Do you want to accept analytics cookies?", visible: :visible)
+    expect(page).to have_field("Yes", type: :radio, visible: :hidden)
+    expect(page).to have_field("No", type: :radio, visible: :hidden)
+    expect(page).to have_button("Save cookie settings", visible: :visible)
+  end
 
-      it "displays the success message" do
-        expect(page).to have_text("You’ve set your cookie preferences.")
-      end
+  def it_checks_the_no_radio_button_by_default
+    expect(page).to have_checked_field("No", visible: :hidden)
+  end
 
-      it "does not display the non-JS message" do
-        expect(page).not_to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
-      end
-    end
+  def it_checks_the_yes_radio_button_by_default
+    expect(page).to have_checked_field("Yes", visible: :hidden)
+  end
 
-    context "when the user has accepted cookies" do
-      before do
-        within_fieldset "Do you want to accept analytics cookies?" do
-          choose "Yes"
-        end
+  def it_does_not_display_the_cookie_consent_form
+    expect(page).not_to have_css("form", visible: :visible)
+  end
 
-        click_button "Save cookie settings"
-      end
+  def it_displays_the_success_message
+    expect(page).to have_text("You’ve set your cookie preferences.")
+  end
 
-      it "loads in Google Analytics" do
-        expect(page).to have_selector('script[src*="googletagmanager"]', visible: :hidden)
-      end
+  def it_does_not_display_the_success_message
+    expect(page).not_to have_text("You’ve set your cookie preferences.")
+  end
 
-      it "does not display the cookie banner" do
-        expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
-      end
+  def it_displays_the_non_js_message
+    expect(page).to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
+  end
 
-      it "displays the cookie consent form" do
-        expect(page).to have_css("legend", text: "Do you want to accept analytics cookies?", visible: :visible)
-        expect(page).to have_checked_field("Yes", visible: :hidden)
-        expect(page).to have_field("No", visible: :hidden)
-        expect(page).to have_button("Save cookie settings")
-      end
-
-      it "displays the success message" do
-        expect(page).to have_text("You’ve set your cookie preferences.")
-      end
-
-      it "does not display the non-JS message" do
-        expect(page).not_to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
-      end
-    end
-
-    context "without javascript enabled", driver: :rack_test do
-      it "does not load in Google Analytics" do
-        expect(page).not_to have_selector('script[src*="googletagmanager"]', visible: :hidden)
-      end
-
-      it "does not display the cookie banner" do
-        expect(page).not_to have_css(".govuk-cookie-banner", visible: :visible)
-      end
-
-      it "does not display the cookie consent form" do
-        expect(page).not_to have_css("form", visible: :visible)
-      end
-
-      it "does not display the success message" do
-        expect(page).not_to have_text("You’ve set your cookie preferences.")
-      end
-
-      it "displays the non-JS message" do
-        expect(page).to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
-      end
-    end
+  def it_does_not_display_the_non_js_message
+    expect(page).not_to have_text("We cannot change your cookie settings at the moment because JavaScript is not running in your browser.")
   end
 end
