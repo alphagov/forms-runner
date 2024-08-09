6160e1ada9f22d6d7c6f9bef0cd25f5c0695efe4
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-08-09 11:11:25 +0100

    Move Page specs into another describe
    The current Page specs are testing using an API call which we never call
    in the application code.
    
    The Page.find method is never used in the application. All forms come
    from the API routes which provide JSON blobs of the form and pages.
    
    The pages spec uses mocks to test other functionality, like answer
    settings.
    
    This commit moves the API mocks into a describe block so features can be
    tested without using the mocks.

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,49 +1,51 @@
 require "rails_helper"
 
 RSpec.describe Page, type: :model do
-  let(:response_data) do
-    {
-      id: 1,
-      question_text: "Question text",
-      answer_type: "date",
-    }.to_json
-  end
-
-  let(:req_headers) do
-    {
-      "X-API-Token" => Settings.forms_api.auth_key,
-      "Accept" => "application/json",
-    }
-  end
-
-  before do
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get "/api/v1/forms/2/pages/1", req_headers, response_data, 200
-    end
-  end
-
-  it "returns the page for a form" do
-    expect(described_class.find(1, params: { form_id: 2 })).to have_attributes(id: 1, question_text: "Question text", answer_type: "date")
-  end
-
-  context "when answer_settings is not present" do
-    it "returns an empty object for answer_settings" do
-      expect(described_class.find(1, params: { form_id: 2 })).to have_attributes(answer_settings: {})
-    end
-  end
-
-  context "when answer_settings is present" do
+  describe "API call" do
     let(:response_data) do
       {
         id: 1,
         question_text: "Question text",
-        answer_type: "selection",
-        answer_settings: { only_one_option: "true" },
+        answer_type: "date",
       }.to_json
     end
 
-    it "returns an answer settings object for answer_settings" do
-      expect(described_class.find(1, params: { form_id: 2 }).answer_settings.attributes).to eq({ "only_one_option" => "true" })
+    let(:req_headers) do
+      {
+        "X-API-Token" => Settings.forms_api.auth_key,
+        "Accept" => "application/json",
+      }
+    end
+
+    before do
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get "/api/v1/forms/2/pages/1", req_headers, response_data, 200
+      end
+    end
+
+    it "returns the page for a form" do
+      expect(described_class.find(1, params: { form_id: 2 })).to have_attributes(id: 1, question_text: "Question text", answer_type: "date")
+    end
+
+    context "when answer_settings is not present" do
+      it "returns an empty object for answer_settings" do
+        expect(described_class.find(1, params: { form_id: 2 })).to have_attributes(answer_settings: {})
+      end
+    end
+
+    context "when answer_settings is present" do
+      let(:response_data) do
+        {
+          id: 1,
+          question_text: "Question text",
+          answer_type: "selection",
+          answer_settings: { only_one_option: "true" },
+        }.to_json
+      end
+
+      it "returns an answer settings object for answer_settings" do
+        expect(described_class.find(1, params: { form_id: 2 }).answer_settings.attributes).to eq({ "only_one_option" => "true" })
+      end
     end
   end
 end
