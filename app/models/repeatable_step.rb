f82ce00185d395da0de6d09b1717c5dc2fc4f1fd
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-08-12 16:24:56 +0100

    Add contents of RepeatableStep
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

--- app/models/repeatable_step.rb
+++ app/models/repeatable_step.rb
@@ -1,2 +1,71 @@
 class RepeatableStep < Step
+  include ActionView::Helpers::SanitizeHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::OutputSafetyHelper
+
+  attr_accessor :answer_id, :questions
+
+  def initialize(...)
+    super
+    @questions = []
+  end
+
+  def repeatable?
+    true
+  end
+
+  def parent_question
+    Step.instance_method(:question).bind(self).call
+  end
+
+  def save_to_context(form_context)
+    form_context.save_step(self, @questions.map(&:serializable_hash))
+    self
+  end
+
+  def load_from_context(form_context)
+    question_attrs = form_context.get_stored_answer(self)
+    @questions = question_attrs.map do |attrs|
+      q = parent_question.dup
+      q.assign_attributes(attrs || {})
+      q
+    end
+
+    self
+  end
+
+  def question
+    if questions.blank?
+      add_blank_answer
+    end
+
+    if next_answer_id == answer_id
+      return add_blank_answer
+    end
+
+    if answer_id && answer_id <= @questions.count
+      return questions[answer_id - 1]
+    end
+
+    questions.first
+  end
+
+  def add_blank_answer
+    questions << parent_question.dup
+    questions[answer_id - 1]
+  end
+
+  def next_answer_id
+    questions.length + 1
+  end
+
+  def show_answer
+    if questions.present?
+      safe_join(
+        questions.map.with_index do |question, index|
+          content_tag(:p, sanitize("#{index + 1}. #{question.show_answer}"))
+        end,
+      )
+    end
+  end
 end
