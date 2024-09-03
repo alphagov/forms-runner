01d9c5ee35889b7441736a74793245d7fe82afde
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-09-03 14:43:52 +0100

    Upload submission to directory in SFTP server

--- app/services/sftp_service.rb
+++ app/services/sftp_service.rb
@@ -3,13 +3,26 @@ require "net/sftp"
 class SftpService
   def upload(file_path, form_id, timestamp, submission_reference)
     Net::SFTP.start("localhost", "test", password: "", port: 2222) do |sftp|
-      remote_path = remote_file_path(form_id, timestamp, submission_reference)
-      sftp.upload!(file_path, remote_path)
+      remote_dir = remote_dir_name(form_id)
+      create_remote_dir(sftp, remote_dir)
+
+      remote_path = remote_file_path(remote_dir, timestamp, submission_reference)
+      sftp.upload!(file_path, remote_path, mkdir: true)
       Rails.logger.info "Uploaded submission to SFTP with path: #{remote_path}"
     end
   end
 
-  def remote_file_path(form_id, timestamp, submission_reference)
-    "#{timestamp.iso8601}_#{submission_reference}.csv"
+  def remote_file_path(remote_dir, timestamp, submission_reference)
+    "#{remote_dir}/#{timestamp.iso8601}_#{submission_reference}.csv"
+  end
+
+  def remote_dir_name(form_id)
+    "form_submissions_#{form_id}"
+  end
+
+  def create_remote_dir(sftp, remote_dir)
+    return if sftp.file.directory? remote_dir
+
+    sftp.mkdir!(remote_dir)
   end
 end
