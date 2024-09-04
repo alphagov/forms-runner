4c2759bb23f59f8670724313cae1cc1c7f244e9a
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-09-04 17:16:23 +0100

    Don't attempt to create directory

--- app/services/sftp_service.rb
+++ app/services/sftp_service.rb
@@ -2,9 +2,9 @@ require "net/sftp"
 
 class SftpService
   def upload(file_path, form_id, timestamp, submission_reference)
-    Net::SFTP.start("localhost", "test", password: "", port: 2222) do |sftp|
+    Net::SFTP.start("localhost", "test", password: "fill me in", port: 2222) do |sftp|
       remote_dir = remote_dir_name(form_id)
-      create_remote_dir(sftp, remote_dir)
+      # create_remote_dir(sftp, remote_dir)
 
       remote_path = remote_file_path(remote_dir, timestamp, submission_reference)
       sftp.upload!(file_path, remote_path, mkdir: true)
@@ -13,10 +13,10 @@ class SftpService
   end
 
   def upload_using_public_key_auth(file_path, form_id, timestamp, submission_reference)
-    private_key = ''
-    Net::SFTP.start("localhost", "test", port: 2222, key_data: [private_key]) do |sftp|
+    private_key_path = "fill me in"
+    Net::SFTP.start("localhost", "test", port: 2222, keys: [private_key_path]) do |sftp|
       remote_dir = remote_dir_name(form_id)
-      create_remote_dir(sftp, remote_dir)
+      # create_remote_dir(sftp, remote_dir)
 
       remote_path = remote_file_path(remote_dir, timestamp, submission_reference)
       sftp.upload!(file_path, remote_path, mkdir: true)
@@ -29,12 +29,14 @@ class SftpService
   end
 
   def remote_dir_name(form_id)
-    "form_submissions_#{form_id}"
+    # "form_submissions_#{form_id}"
+    # Directory will be stored on form to avoid having to create?
+    "form_submissions_5"
   end
 
-  def create_remote_dir(sftp, remote_dir)
-    return if sftp.file.directory? remote_dir
-
-    sftp.mkdir!(remote_dir)
-  end
+  # def create_remote_dir(sftp, remote_dir)
+  #   return if sftp.file.directory? remote_dir
+  #
+  #   sftp.mkdir!(remote_dir)
+  # end
 end
