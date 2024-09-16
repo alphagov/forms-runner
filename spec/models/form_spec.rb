e7fa9bc69787b37524968407903b15ee26dfe999
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-09-16 17:03:37 +0300

    Stop testing /forms/:id endpoint
    We have a bunch of tests that invoke the `/forms/:id` endpoint, but we
    don't use that endpoint in our app code, and we want to deprecate it in
    the API in future.
    
    Rewrite these tests so that they are using the `/forms/:id/{draft, live}`
    endpoints instead.

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -25,76 +25,38 @@ RSpec.describe Form, type: :model do
     end
   end
 
-  it "returns a simple form" do
-    expect(form).to have_attributes(id: 1, name: "form name", submission_email: "user@example.com")
-  end
-
-  describe "Getting the pages for a form" do
-    it "returns the pages for a form" do
-      pages = form.pages
-      expect(pages.length).to eq(2)
-      expect(pages[0]).to have_attributes(id: 9, next_page: 10, answer_type: "date", question_text: "Question one")
-      expect(pages[1]).to have_attributes(id: 10, answer_type: "address", question_text: "Question two")
+  shared_examples "form snapshot" do
+    it "returns a simple form" do
+      expect(form).to have_attributes(id: 1, name: "form name", submission_email: "user@example.com")
     end
-  end
 
-  describe "#live?" do
-    let(:response_data) { { id: 1, name: "form name", live_at: }.compact }
-    let(:live_at) { "2022-08-18 09:16:50Z" }
-
-    context "when live_at is not set" do
-      let(:live_at) { nil }
-
-      it "raises an error" do
-        expect { form.live? }.to raise_error(Date::Error)
+    describe "#pages" do
+      it "returns the pages for the form" do
+        pages = form.pages
+        expect(pages.length).to eq(2)
+        expect(pages[0]).to have_attributes(id: 9, next_page: 10, answer_type: "date", question_text: "Question one")
+        expect(pages[1]).to have_attributes(id: 10, answer_type: "address", question_text: "Question two")
       end
     end
 
-    context "when live_at is set to empty string" do
-      let(:live_at) { "" }
+    describe "#payment_url_with_reference" do
+      let(:response_data) { { id: 1, name: "form name", payment_url:, start_page: 1 } }
+      let(:reference) { SecureRandom.base58(8).upcase }
 
-      it "returns false" do
-        expect(form.live?).to be false
+      context "when there is a payment_url" do
+        let(:payment_url) { "https://www.gov.uk/payments/test-service/pay-for-licence" }
+
+        it "returns a full payment link" do
+          expect(form.payment_url_with_reference(reference)).to eq("#{payment_url}?reference=#{reference}")
+        end
       end
-    end
 
-    context "when live_at is a string which isn't a valid date" do
-      let(:live_at) { "not a date!" }
+      context "when there is no payment_url" do
+        let(:payment_url) { nil }
 
-      it "raises an error" do
-        expect { form.live? }.to raise_error(Date::Error)
-      end
-    end
-
-    context "when live_at is not a string" do
-      let(:live_at) { 1 }
-
-      it "raises an error" do
-        expect { form.live? }.to raise_error(Date::Error)
-      end
-    end
-
-    context "when live_at is a date in the future" do
-      let(:live_at) { "2022-08-18 09:16:50Z" }
-
-      it "returns false" do
-        expect(form.live?("2022-01-01 10:00:00Z")).to be false
-      end
-    end
-
-    context "when live_at is a date in the past" do
-      let(:live_at) { "2022-08-18 09:16:50Z" }
-
-      it "returns true" do
-        expect(form.live?("2023-01-01 10:00:00Z")).to be true
-      end
-    end
-
-    context "when dates are the same" do
-      let(:live_at) { "2022-08-18 09:16:50Z" }
-
-      it "returns false" do
-        expect(form.live?("2022-08-18 09:16:50Z")).to be false
+        it "returns nil" do
+          expect(form.payment_url_with_reference(reference)).to be_nil
+        end
       end
     end
   end
@@ -102,7 +64,7 @@ RSpec.describe Form, type: :model do
   describe "#find_with_mode" do
     context "when mode is live" do
       let(:form) { described_class.find_with_mode(id: 1, mode: Mode.new("live")) }
-      let(:response_data) { { id: 1, name: "form name", live_at: "2022-08-18 09:16:50Z" } }
+      let(:response_data) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: "2022-08-18 09:16:50Z", pages: } }
 
       before do
         ActiveResource::HttpMock.respond_to do |mock|
@@ -110,21 +72,86 @@ RSpec.describe Form, type: :model do
         end
       end
 
+      it_behaves_like "form snapshot"
+
       it "returns a live form" do
         expect(form).to have_attributes(id: 1, name: "form name")
         expect(form.live?).to be(true)
+      end
+
+      describe "#live?" do
+        let(:response_data) { { id: 1, name: "form name", live_at: }.compact }
+        let(:live_at) { "2022-08-18 09:16:50Z" }
+
+        context "when live_at is not set" do
+          let(:live_at) { nil }
+
+          it "raises an error" do
+            expect { form.live? }.to raise_error(Date::Error)
+          end
+        end
+
+        context "when live_at is set to empty string" do
+          let(:live_at) { "" }
+
+          it "returns false" do
+            expect(form.live?).to be false
+          end
+        end
+
+        context "when live_at is a string which isn't a valid date" do
+          let(:live_at) { "not a date!" }
+
+          it "raises an error" do
+            expect { form.live? }.to raise_error(Date::Error)
+          end
+        end
+
+        context "when live_at is not a string" do
+          let(:live_at) { 1 }
+
+          it "raises an error" do
+            expect { form.live? }.to raise_error(Date::Error)
+          end
+        end
+
+        context "when live_at is a date in the future" do
+          let(:live_at) { "2022-08-18 09:16:50Z" }
+
+          it "returns false" do
+            expect(form.live?("2022-01-01 10:00:00Z")).to be false
+          end
+        end
+
+        context "when live_at is a date in the past" do
+          let(:live_at) { "2022-08-18 09:16:50Z" }
+
+          it "returns true" do
+            expect(form.live?("2023-01-01 10:00:00Z")).to be true
+          end
+        end
+
+        context "when dates are the same" do
+          let(:live_at) { "2022-08-18 09:16:50Z" }
+
+          it "returns false" do
+            expect(form.live?("2022-08-18 09:16:50Z")).to be false
+          end
+        end
       end
     end
 
     context "when mode is draft" do
       let(:form) { described_class.find_with_mode(id: 1, mode: Mode.new("preview-draft")) }
-      let(:response_data) { { id: 1, name: "form name", live_at: nil } }
+      let(:response_data) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: nil, pages: } }
 
       before do
         ActiveResource::HttpMock.respond_to do |mock|
           mock.get "/api/v1/forms/1/draft", req_headers, response_data.to_json, 200
         end
       end
+
+      it_behaves_like "form snapshot"
 
       it "returns a draft form" do
         expect(form).to have_attributes(id: 1, name: "form name")
@@ -158,27 +185,6 @@ RSpec.describe Form, type: :model do
 
         expect(form).to have_attributes(id: "Alpha123", name: "form name")
         expect(form.live?).to be(false)
-      end
-    end
-  end
-
-  describe "#payment_url_with_reference" do
-    let(:response_data) { { id: 1, name: "form name", payment_url:, start_page: 1 } }
-    let(:reference) { SecureRandom.base58(8).upcase }
-
-    context "when there is a payment_url" do
-      let(:payment_url) { "https://www.gov.uk/payments/test-service/pay-for-licence" }
-
-      it "returns a full payment link" do
-        expect(form.payment_url_with_reference(reference)).to eq("#{payment_url}?reference=#{reference}")
-      end
-    end
-
-    context "when there is no payment_url" do
-      let(:payment_url) { nil }
-
-      it "returns nil" do
-        expect(form.payment_url_with_reference(reference)).to be_nil
       end
     end
   end
