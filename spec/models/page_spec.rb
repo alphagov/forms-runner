db8d3042f49708027837d61b446be45432432083
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-09-16 15:14:34 +0300

    Refactor Page model specs
    - Simplify Page model factory
    - Use described class instead of factory for model specs

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -8,29 +8,29 @@ RSpec.describe Page, type: :model do
 
   describe "#answer_settings" do
     it "returns an empty object for answer_settings when it's not present" do
-      page = build :page
+      page = described_class.new
       expect(page).to have_attributes(answer_settings: {})
     end
 
     it "returns an answer settings object for answer_settings when present" do
-      page = build :page, answer_settings: { only_one_option: "true" }
+      page = described_class.new(answer_settings: { only_one_option: "true" })
       expect(page.answer_settings.attributes).to eq({ "only_one_option" => "true" })
     end
   end
 
   describe "#repeatable?" do
     it "returns false when attribute does not exist" do
-      page = build :page
+      page = described_class.new
       expect(page.repeatable?).to be false
     end
 
     it "returns false when attribute is false" do
-      page = build :page, is_repeatable: false
+      page = described_class.new is_repeatable: false
       expect(page.repeatable?).to be false
     end
 
     it "returns true when attribute is true" do
-      page = build :page, is_repeatable: true
+      page = described_class.new is_repeatable: true
       expect(page.repeatable?).to be true
     end
   end
