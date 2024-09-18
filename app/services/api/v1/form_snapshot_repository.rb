8497a728bd7be4a766e6e6f7ffed2cd4a2518315
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-09-18 12:39:14 +0300

    Allow using v2 API
    Change `Api::V1::FormSnapshotRepository` to use
    `Api::V2::FormDocumentResource` and `Api::V1::Converter` to allow this
    app to retrieve forms using the v2 API without changes to the rest of
    the codebase. Basically, we get the v2 form documents and then
    immediately convert them to the older form snapshot format so we can
    keep using the same models.
    
    This behaviour is currently behind a feature flag and not enabled.

--- app/services/api/v1/form_snapshot_repository.rb
+++ app/services/api/v1/form_snapshot_repository.rb
@@ -1,7 +1,37 @@
 class Api::V1::FormSnapshotRepository
   class << self
     def find_with_mode(id:, mode:)
-      Form.find_with_mode(id:, mode:)
+      if FeatureService.enabled? :api_v2
+        v2_find_with_mode(id:, mode:)
+      else
+        Form.find_with_mode(id:, mode:)
+      end
+    end
+
+  private
+
+    def converter
+      @converter ||= Api::V1::Converter.new
+    end
+
+    def v2_find_with_mode(id:, mode:)
+      raise ActiveResource::ResourceNotFound.new(404, "Not Found") unless id.to_s =~ /^[[:alnum:]]+$/
+
+      tag = if mode.preview_draft?
+              :draft
+            elsif mode.preview_archived?
+              :archived
+            elsif mode.live?
+              :live
+            elsif mode.preview_live?
+              :live
+            end
+
+      v2_form_document = Api::V2::FormDocumentResource.find(id, tag)
+      v2_blob = v2_form_document.as_json
+
+      v1_blob = converter.to_api_v1_form_snapshot(v2_blob)
+      Form.new(v1_blob, true)
     end
   end
 end
