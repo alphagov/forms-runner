7daf448b37381a32bfa85a23035fb8d643ea9919
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-09-18 19:25:05 +0300

    Add factories for v2 API form responses

---
+++ spec/factories/v2_form_document.rb
@@ -0,0 +1,63 @@
+FactoryBot.define do
+  factory :v2_form_document, class: DataStruct do
+    form_id { Faker::Alphanumeric.alphanumeric(number: 8) }
+
+    sequence(:name) { |n| "Form #{n}" }
+    form_slug { name ? name.parameterize : nil }
+
+    steps { [] }
+
+    declaration_text { nil }
+    declaration_section_completed { false }
+    payment_url { nil }
+    privacy_policy_url { nil }
+    submission_email { nil }
+    submission_type { nil }
+    support_email { nil }
+    support_phone { nil }
+    support_url { nil }
+    support_url_text { nil }
+    question_section_completed { false }
+    what_happens_next_markdown { nil }
+
+    trait :with_steps do
+      transient do
+        steps_count { 5 }
+      end
+
+      steps do
+        Array.new(steps_count) { attributes_for(:v2_step) }
+      end
+
+      question_section_completed { true }
+    end
+
+    trait :with_privacy_policy_url do
+      privacy_policy_url { Faker::Internet.url host: "gov.uk" }
+    end
+
+    trait :with_submission_email do
+      submission_email { Faker::Internet.email domain: "example.gov.uk" }
+    end
+
+    trait :with_support do
+      support_email { Faker::Internet.email(domain: "example.gov.uk") }
+      support_phone { Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4) }
+      support_url { Faker::Internet.url(host: "gov.uk") }
+      support_url_text { Faker::Lorem.sentence(word_count: 1, random_words_to_add: 4) }
+    end
+
+    trait :ready_for_live do
+      with_steps
+      with_privacy_policy_url
+      with_submission_email
+      support_email { Faker::Internet.email(domain: "example.gov.uk") }
+      what_happens_next_markdown { "We usually respond to applications within 10 working days." }
+    end
+
+    trait :live? do
+      ready_for_live
+      live_at { Time.zone.now }
+    end
+  end
+end
---
+++ spec/factories/v2_step.rb
@@ -0,0 +1,72 @@
+FactoryBot.define do
+  factory :v2_step, class: DataStruct do
+    id { Faker::Number.number(digits: 2) }
+
+    sequence(:position) { |n| n }
+    next_step_id { nil }
+
+    routing_conditions { [] }
+
+    type { nil }
+    data { nil }
+
+    factory :v2_question_page_step do
+      type { "question_page" }
+
+      transient do
+        answer_type { "number" }
+        answer_settings { nil }
+
+        question_text { Faker::Lorem.question }
+
+        hint_text { nil }
+
+        guidance_markdown { nil }
+        page_heading { nil }
+
+        is_optional { false }
+        is_repeatable { false }
+      end
+
+      data do
+        DataStruct.new(
+          answer_settings:,
+          answer_type:,
+          guidance_markdown:,
+          hint_text:,
+          is_optional:,
+          is_repeatable:,
+          page_heading:,
+          question_text:,
+        )
+      end
+
+      trait :with_selections_settings do
+        transient do
+          only_one_option { "true" }
+          selection_options { [{ name: "Option 1" }, { name: "Option 2" }] }
+        end
+
+        answer_type { "selection" }
+        answer_settings { DataStruct.new(only_one_option:, selection_options:) }
+      end
+
+      trait :with_text_settings do
+        transient do
+          input_type { %w[single_line long_text].sample }
+        end
+
+        answer_type { "text" }
+        answer_settings do
+          DataStruct.new(
+            input_type:,
+          )
+        end
+      end
+
+      trait :with_repeatable do
+        is_repeatable { true }
+      end
+    end
+  end
+end
