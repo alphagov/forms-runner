bea56c32c0178c7a0e03b889103f1324dff0bad0
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-10-03 12:50:50 +0300

    Add comments to Flow::Journey class
    It's not immediately obvious what the Journey class if for or how it works.

--- app/lib/flow/journey.rb
+++ app/lib/flow/journey.rb
@@ -1,4 +1,21 @@
 module Flow
+  ##
+  # This class represents the journey taken by a form filler through a form.
+  #
+  # For a simple form (with no routing) there is only one possible set of
+  # steps that a form filler can take, but for a form with routing there
+  # may be some pages that the form filler never sees.
+  #
+  # Journey#completed_steps is an array of the steps that the form filler has
+  # visited so far in the form, in the order defined by the form. If their
+  # answer to a question page step causes a routing rule to be applied, for
+  # instance by skipping over the next two questions, only the questions in the
+  # resulting route are included.
+  #
+  # Note: the completed_steps array is ordered, from start to last step
+  # answered; if the form filler has not yet visited the forms first page the
+  # array will be empty.
+
   class Journey
     include Enumerable
 
@@ -12,8 +29,11 @@ module Flow
 
   private
 
-    def find_existing_step(page_slug)
+    def find_completed_step(page_slug)
       step = @step_factory.create_step(page_slug)
+
+      # A step has been completed if it is a question page that has been answered.
+      # We also need to load the answer into the step for next_page_with_routing to give the correct result.
       step.load_from_context(@form_context) unless @form_context.get_stored_answer(step).nil?
     rescue ActiveModel::UnknownAttributeError, ArgumentError
       @form_context.clear_stored_answer(step)
@@ -22,7 +42,7 @@ module Flow
 
     def generate_completed_steps
       @completed_steps = []
-      current_step = find_existing_step(:_start)
+      current_step = find_completed_step(:_start)
 
       while current_step
         next_page_slug = current_step.next_page_slug_after_routing
@@ -34,7 +54,7 @@ module Flow
 
         break if next_page_slug.nil?
 
-        current_step = find_existing_step(next_page_slug)
+        current_step = find_completed_step(next_page_slug)
       end
     end
   end
