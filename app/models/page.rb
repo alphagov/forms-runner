b9823c1c8f55f4561bbe8d3a03f0c30c277dd2ac
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-10-15 10:04:52 +0300

    Remove code using V1 API
    We are no longer using the V1 API code anywhere, and are happy with the
    functioning of the V2 API for forms-runner. We can delete this code.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,20 +2,10 @@ class Page < ActiveResource::Base
   PAGE_ID_REGEX = /\d+/
 
   self.site = Settings.forms_api.base_url
-  self.prefix = "/api/v1/forms/:form_id/"
+  self.prefix = "/api/v2/forms/:form_id/"
   self.include_format_in_path = false
-  headers["X-API-Token"] = Settings.forms_api.auth_key
 
   belongs_to :form
-
-  def self.deprecator
-    Rails.application.deprecators[:forms_api]
-  end
-
-  def self.find(...)
-    deprecator.warn "the /forms/:id/pages endpoints are deprecated and will be removed in API v2"
-    super
-  end
 
   def form_id
     @prefix_options[:form_id]
--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,36 +1,15 @@
 require "rails_helper"
 
-RSpec.describe Form, feature_api_v2: false, type: :model do
-  let(:form) { described_class.find(1) }
-  let(:response_data) { { id: 1, name: "form name", submission_email: "user@example.com", start_page: 1 } }
+RSpec.describe Form, type: :model do
+  subject(:form) { described_class.new(attributes) }
+
+  let(:attributes) { { id: 1, name: "form name", submission_email: "user@example.com", start_page: 1, pages: } }
 
   let(:pages) do
     [
       { id: 9, next_page: 10, answer_type: "date", question_text: "Question one" },
       { id: 10, answer_type: "address", question_text: "Question two" },
     ]
-  end
-
-  let(:req_headers) do
-    {
-      "X-API-Token" => Settings.forms_api.auth_key,
-      "Accept" => "application/json",
-    }
-  end
-
-  before do
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get "/api/v1/forms/1", req_headers, response_data.to_json, 200
-      mock.get "/api/v1/forms/1/pages", req_headers, pages.to_json, 200
-    end
-  end
-
-  describe "getting a form without a mode" do
-    it "raises a deprecation warning" do
-      expect {
-        form
-      }.to raise_error(ActiveSupport::DeprecationException)
-    end
   end
 
   shared_examples "form snapshot" do
@@ -48,7 +27,7 @@ RSpec.describe Form, feature_api_v2: false, type: :model do
     end
 
     describe "#payment_url_with_reference" do
-      let(:response_data) { { id: 1, name: "form name", payment_url:, start_page: 1 } }
+      let(:attributes) { { id: 1, name: "form name", payment_url:, start_page: 1 } }
       let(:reference) { SecureRandom.base58(8).upcase }
 
       context "when there is a payment_url" do
@@ -69,129 +48,84 @@ RSpec.describe Form, feature_api_v2: false, type: :model do
     end
   end
 
-  describe "#find_with_mode" do
-    context "when mode is live" do
-      let(:form) { described_class.find_with_mode(id: 1, mode: Mode.new("live")) }
-      let(:response_data) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: "2022-08-18 09:16:50Z", pages: } }
+  context "when mode is live" do
+    let(:attributes) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: "2022-08-18 09:16:50Z", pages: } }
 
-      before do
-        ActiveResource::HttpMock.respond_to do |mock|
-          mock.get "/api/v1/forms/1/live", req_headers, response_data.to_json, 200
+    it_behaves_like "form snapshot"
+
+    it "returns a live form" do
+      expect(form).to have_attributes(id: 1, name: "form name")
+      expect(form.live?).to be(true)
+    end
+
+    describe "#live?" do
+      let(:attributes) { { id: 1, name: "form name", live_at: }.compact }
+      let(:live_at) { "2022-08-18 09:16:50Z" }
+
+      context "when live_at is not set" do
+        let(:live_at) { nil }
+
+        it "raises an error" do
+          expect { form.live? }.to raise_error(Date::Error)
         end
       end
 
-      it_behaves_like "form snapshot"
+      context "when live_at is set to empty string" do
+        let(:live_at) { "" }
 
-      it "returns a live form" do
-        expect(form).to have_attributes(id: 1, name: "form name")
-        expect(form.live?).to be(true)
+        it "returns false" do
+          expect(form.live?).to be false
+        end
       end
 
-      describe "#live?" do
-        let(:response_data) { { id: 1, name: "form name", live_at: }.compact }
+      context "when live_at is a string which isn't a valid date" do
+        let(:live_at) { "not a date!" }
+
+        it "raises an error" do
+          expect { form.live? }.to raise_error(Date::Error)
+        end
+      end
+
+      context "when live_at is not a string" do
+        let(:live_at) { 1 }
+
+        it "raises an error" do
+          expect { form.live? }.to raise_error(Date::Error)
+        end
+      end
+
+      context "when live_at is a date in the future" do
         let(:live_at) { "2022-08-18 09:16:50Z" }
 
-        context "when live_at is not set" do
-          let(:live_at) { nil }
-
-          it "raises an error" do
-            expect { form.live? }.to raise_error(Date::Error)
-          end
+        it "returns false" do
+          expect(form.live?("2022-01-01 10:00:00Z")).to be false
         end
+      end
 
-        context "when live_at is set to empty string" do
-          let(:live_at) { "" }
+      context "when live_at is a date in the past" do
+        let(:live_at) { "2022-08-18 09:16:50Z" }
 
-          it "returns false" do
-            expect(form.live?).to be false
-          end
+        it "returns true" do
+          expect(form.live?("2023-01-01 10:00:00Z")).to be true
         end
+      end
 
-        context "when live_at is a string which isn't a valid date" do
-          let(:live_at) { "not a date!" }
+      context "when dates are the same" do
+        let(:live_at) { "2022-08-18 09:16:50Z" }
 
-          it "raises an error" do
-            expect { form.live? }.to raise_error(Date::Error)
-          end
-        end
-
-        context "when live_at is not a string" do
-          let(:live_at) { 1 }
-
-          it "raises an error" do
-            expect { form.live? }.to raise_error(Date::Error)
-          end
-        end
-
-        context "when live_at is a date in the future" do
-          let(:live_at) { "2022-08-18 09:16:50Z" }
-
-          it "returns false" do
-            expect(form.live?("2022-01-01 10:00:00Z")).to be false
-          end
-        end
-
-        context "when live_at is a date in the past" do
-          let(:live_at) { "2022-08-18 09:16:50Z" }
-
-          it "returns true" do
-            expect(form.live?("2023-01-01 10:00:00Z")).to be true
-          end
-        end
-
-        context "when dates are the same" do
-          let(:live_at) { "2022-08-18 09:16:50Z" }
-
-          it "returns false" do
-            expect(form.live?("2022-08-18 09:16:50Z")).to be false
-          end
+        it "returns false" do
+          expect(form.live?("2022-08-18 09:16:50Z")).to be false
         end
       end
     end
 
     context "when mode is draft" do
-      let(:form) { described_class.find_with_mode(id: 1, mode: Mode.new("preview-draft")) }
-      let(:response_data) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: nil, pages: } }
-
-      before do
-        ActiveResource::HttpMock.respond_to do |mock|
-          mock.get "/api/v1/forms/1/draft", req_headers, response_data.to_json, 200
-        end
-      end
+      let(:attributes) { { id: 1, name: "form name", submission_email: "user@example.com", live_at: nil, pages: } }
 
       it_behaves_like "form snapshot"
 
       it "returns a draft form" do
         expect(form).to have_attributes(id: 1, name: "form name")
-        expect(form.live?).to be(false)
-      end
-    end
-
-    context "when validating the provided form id" do
-      let(:response_data) { { id: "Alpha123", name: "form name", live_at: nil } }
-
-      before do
-        ActiveResource::HttpMock.respond_to do |mock|
-          mock.get "/api/v1/forms/Alpha123/draft", req_headers, response_data.to_json, 200
-        end
-      end
-
-      it "returns ResourceNotFound when the id contains non-alpha-numeric chars" do
-        expect {
-          described_class.find_with_mode(id: "<id>", mode: Mode.new("preview-draft"))
-        }.to raise_error(ActiveResource::ResourceNotFound)
-      end
-
-      it "returns ResourceNotFound when the id is blank" do
-        expect {
-          described_class.find_with_mode(id: "", mode: Mode.new("preview-draft"))
-        }.to raise_error(ActiveResource::ResourceNotFound)
-      end
-
-      it "returns the form when the id is alphanumeric" do
-        form = described_class.find_with_mode(id: "Alpha123", mode: Mode.new("preview-draft"))
-
-        expect(form).to have_attributes(id: "Alpha123", name: "form name")
         expect(form.live?).to be(false)
       end
     end
