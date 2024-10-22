48f58f55b2b4b9949bc3d5b98e4072c161ac1f02
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-10-22 10:31:04 +0100

    Add tests for Step model
    The Step model doesn't have any unit tests.
    
    This makes changing the class harder:
    - it's tricky to know we haven't broken something the code relies on
    - new methods are less likely to have tests written for them if there is
      no exsiting spec file to add to
    
    This commit adds a spec file for the Step model.
    
    The tests are writted to match the existing code and the functionality
    it currently has.
    
    There are ways we could improve the Step model in the future which would
    make the tests easier to write and maintain.
    
    These tests will be the base on which improvements can be made.
    
    For example, we could:
    - make some methods in the Step model private
    - remove any methods which are not used in the code
    - improve the way the Step model is instantiated to make it easier to
      configure and test
    - add a model for conditions, which is currently an implicit class
      created by ActiveResource
    - clear up the factories to make them easier to use in tests, rather
      than using doubles

---
+++ spec/models/step_spec.rb
@@ -0,0 +1,256 @@
+require "rails_helper"
+
+RSpec.describe Step do
+  subject(:step) do
+    described_class.new(
+      question:,
+      page:,
+      form_id: 3,
+      form_slug: "test-form",
+      next_page_slug: "next-page",
+      page_slug: "current-page",
+    )
+  end
+
+  let(:question) { instance_double(Question::Text, serializable_hash: {}, attribute_names: %w[name], valid?: true) }
+  let(:page) { build(:page, id: 2, position: 1, routing_conditions: []) }
+  let(:form_context) { instance_double(Flow::FormContext) }
+
+  describe "#initialize" do
+    it "sets the attributes correctly" do
+      expect(step.question).to eq(question)
+      expect(step.page_id).to eq(2)
+      expect(step.page_slug).to eq("current-page")
+      expect(step.form_id).to eq(3)
+      expect(step.form_slug).to eq("test-form")
+      expect(step.next_page_slug).to eq("next-page")
+      expect(step.page_number).to eq(1)
+      expect(step.routing_conditions).to eq([])
+    end
+  end
+
+  describe "#==" do
+    it "returns true for steps with the same state" do
+      other_step = described_class.new(
+        question:,
+        page:,
+        form_id: 3,
+        form_slug: "test-form",
+        next_page_slug: "next-page",
+        page_slug: "current-page",
+      )
+      expect(step).to eq(other_step)
+    end
+
+    it "returns false for steps with different states" do
+      other_step = described_class.new(
+        question:,
+        page:,
+        form_id: 4,
+        form_slug: "other-form",
+        next_page_slug: "other-page",
+        page_slug: "other-current-page",
+      )
+      expect(step == other_step).to be false
+    end
+  end
+
+  describe "#state" do
+    it "returns an array of instance variable values" do
+      expected_state = [
+        step.question,
+        step.page_id,
+        step.page_slug,
+        step.form_id,
+        step.form_slug,
+        step.next_page_slug,
+        step.page_number,
+        step.routing_conditions,
+      ]
+
+      expect(step.state).to match_array(expected_state)
+    end
+
+    it "changes when an instance variable is modified" do
+      original_state = step.state.dup
+      step.form_id = "new_form_id"
+      expect(step.state).not_to eq(original_state)
+    end
+  end
+
+  describe "#save_to_context" do
+    it "saves the step to the form context" do
+      expect(form_context).to receive(:save_step).with(step, {})
+      step.save_to_context(form_context)
+    end
+  end
+
+  describe "#load_from_context" do
+    it "loads the step from the form context" do
+      allow(form_context).to receive(:get_stored_answer).with(step).and_return({ name: "Test" })
+      expect(question).to receive(:assign_attributes).with({ name: "Test" })
+      step.load_from_context(form_context)
+    end
+  end
+
+  describe "#update!" do
+    it "assigns attributes and validates the question" do
+      params = { name: "New Name" }
+      expect(question).to receive(:assign_attributes).with(params)
+      expect(question).to receive(:valid?)
+      step.update!(params)
+    end
+  end
+
+  describe "#params" do
+    it "returns question attribute names with selection" do
+      expect(step.params).to eq(["name", { selection: [] }])
+    end
+  end
+
+  describe "#clear_errors" do
+    it "clears errors on the question" do
+      errors = instance_double(ActiveModel::Errors)
+      allow(question).to receive(:errors).and_return(errors)
+      expect(errors).to receive(:clear)
+      step.clear_errors
+    end
+  end
+
+  describe "#end_page?" do
+    context "when next_page_slug is nil" do
+      subject(:step) do
+        described_class.new(question:, page:, form_id: 3, form_slug: "test-form", next_page_slug: nil, page_slug: "current-page")
+      end
+
+      it { is_expected.to be_end_page }
+    end
+
+    context "when next_page_slug is not nil" do
+      it { is_expected.not_to be_end_page }
+    end
+  end
+
+  describe "#next_page_slug_after_routing" do
+    context "with matching routing conditions" do
+      let(:question) { instance_double(Question::Selection, selection: "Yes") }
+      let(:page) { build(:page, id: 2, position: 1, routing_conditions: [OpenStruct.new(answer_value: "Yes", goto_page_id: "5")]) }
+
+      it "returns the goto_page_slug" do
+        expect(step.next_page_slug_after_routing).to eq("5")
+      end
+    end
+
+    context "with multiple routing conditions" do
+      let(:page) do
+        build(:page, id: 2, position: 1, routing_conditions: [
+          OpenStruct.new(answer_value: "Yes", goto_page_id: "5"),
+          OpenStruct.new(answer_value: "No", goto_page_id: "6"),
+        ])
+      end
+
+      let(:question) { instance_double(Question::Selection, selection: "Yes") }
+
+      it "returns the correct goto_page_id based on selection" do
+        expect(step.next_page_slug_after_routing).to eq("5")
+      end
+    end
+
+    context "without matching routing conditions" do
+      it "returns the next_page_slug" do
+        expect(step.next_page_slug_after_routing).to eq("next-page")
+      end
+    end
+  end
+
+  describe "#goto_page_slug" do
+    context "with routing conditions" do
+      let(:page) { build(:page, id: 2, position: 1, routing_conditions: [OpenStruct.new(goto_page_id: "5", skip_to_end: false)]) }
+
+      it "returns the goto_page_id as a string" do
+        expect(step.goto_page_slug).to eq("5")
+      end
+    end
+
+    context "without routing conditions" do
+      it "returns nil" do
+        expect(step.goto_page_slug).to be_nil
+      end
+    end
+
+    context "with skip_to_end condition" do
+      let(:page) { build(:page, id: 2, position: 1, routing_conditions: [OpenStruct.new(goto_page_id: nil, skip_to_end: true)]) }
+
+      it "returns the check your answers page slug" do
+        expect(step.goto_page_slug).to eq(CheckYourAnswersStep::CHECK_YOUR_ANSWERS_PAGE_SLUG)
+      end
+    end
+  end
+
+  describe "#repeatable?" do
+    it "returns false" do
+      expect(step.repeatable?).to be false
+    end
+  end
+
+  describe "#skipped?" do
+    it "returns true if question is optional and show_answer is blank" do
+      allow(question).to receive_messages(
+        is_optional?: true,
+        show_answer: "",
+      )
+      expect(step.skipped?).to be true
+    end
+
+    it "returns false if is_optional? is false" do
+      allow(question).to receive_messages(
+        is_optional?: false,
+        show_answer: "",
+      )
+      expect(step.skipped?).to be false
+    end
+
+    it "returns false if show_answer is not blank" do
+      allow(question).to receive_messages(
+        is_optional?: true,
+        show_answer: "something",
+      )
+      expect(step.skipped?).to be false
+    end
+  end
+
+  shared_examples "delegates to question" do |method_name|
+    it "delegates #{method_name} to question" do
+      expect(question).to receive(method_name)
+      step.send(method_name)
+    end
+  end
+
+  describe "#valid?" do
+    it_behaves_like "delegates to question", :valid?
+  end
+
+  describe "#show_answer" do
+    it_behaves_like "delegates to question", :show_answer
+  end
+
+  describe "#show_answer_in_email" do
+    it_behaves_like "delegates to question", :show_answer_in_email
+  end
+
+  describe "#show_answer_in_csv" do
+    it_behaves_like "delegates to question", :show_answer_in_csv
+  end
+
+  describe "#question_text" do
+    it_behaves_like "delegates to question", :question_text
+  end
+
+  describe "#hint_text" do
+    it_behaves_like "delegates to question", :hint_text
+  end
+
+  describe "#answer_settings" do
+    it_behaves_like "delegates to question", :answer_settings
+  end
+end
