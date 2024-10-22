75d3cf0a57f03050dfbe4e36bb797eb07e9041f6
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-10-22 17:17:11 +0100

    Specify S3 bucket region
    When using the AWS SDK for Ruby, it is required to provide the bucket
    region. If the region is different to the region the bucket was
    created in, we get the following error:
    
    Aws::s3::Errors::PermanentRedirect - The bucket you are attempting to
    access must be addressed using the specified endpoint. Please send all
    future requests to this endpoint.
    
    We now store the s3_bucket_region on the form. Use this when
    initializing the S3 client so that we make requests to S3 using the
    region for the bucket.

--- app/services/s3_submission_service.rb
+++ app/services/s3_submission_service.rb
@@ -3,21 +3,25 @@ class S3SubmissionService
                  form_id:,
                  s3_bucket_name:,
                  s3_bucket_aws_account_id:,
+                 s3_bucket_region:,
                  timestamp:,
                  submission_reference:)
     raise ArgumentError, "s3_bucket_name cannot be nil" if s3_bucket_name.nil?
     raise ArgumentError, "s3_bucket_aws_account_id cannot be nil" if s3_bucket_aws_account_id.nil?
+    raise ArgumentError, "s3_bucket_region cannot be nil" if s3_bucket_region.nil?
 
     @file_path = file_path
     @form_id = form_id
     @bucket = s3_bucket_name
     @expected_bucket_owner = s3_bucket_aws_account_id
+    @region = s3_bucket_region
     @timestamp = timestamp
     @submission_reference = submission_reference
   end
 
   def upload_file_to_s3
     s3 = Aws::S3::Client.new(
+      region: @region,
       credentials: assume_role,
     )
     s3.put_object({
