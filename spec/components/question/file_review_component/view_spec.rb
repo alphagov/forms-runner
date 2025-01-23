4234c7eb6a0e905ad9be08c7931888d3f25f42c0
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-01-23 10:43:51 +0000

    Add file review component
    Add a component containing the table to review the uploaded file name
    along with a remove button for file upload questions. We're using a
    component that inherits the Question::Base component as we need to use
    the methods on this to render the question text heading and hint text,
    if it's present.
    
    Co-authored-by: Jamie Wilkinson <jamie.wilkinson1@digital.cabinet-office.gov.uk>

---
+++ spec/components/question/file_review_component/view_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe Question::FileReviewComponent::View, type: :component do
+  let(:question_page) { build :page, answer_type: "file" }
+  let(:is_optional) { false }
+  let(:question_text) { "Upload a file" }
+  let(:page_heading) { nil }
+  let(:hint_text) { nil }
+  let(:question) { build :file, :with_uploaded_file, question_text:, is_optional:, page_heading:, hint_text: }
+  let(:extra_question_text_suffix) { nil }
+  let(:remove_file_url) { "/remove_file" }
+
+  before do
+    render_inline(described_class.new(question:, extra_question_text_suffix:, remove_file_url:))
+  end
+
+  context "when there is no page heading" do
+    it "renders the question text as a h1" do
+      expect(page.find("h1")).to have_text(question.question_text_with_optional_suffix)
+    end
+  end
+
+  context "when there is a page heading" do
+    let(:page_heading) { Faker::Lorem.sentence }
+
+    it "renders the question text as a h2" do
+      expect(page.find("h2")).to have_text(question.question_text_with_optional_suffix)
+    end
+  end
+
+  context "when the question has hint text" do
+    let(:hint_text) { Faker::Lorem.sentence }
+
+    it "outputs the hint text" do
+      expect(page.find(".govuk-hint")).to have_text(hint_text)
+    end
+  end
+
+  context "when the question has no hint text" do
+    it "does not output the hint text" do
+      expect(page).not_to have_css(".govuk-hint")
+    end
+  end
+
+  it "renders the original file name" do
+    expect(page).to have_content(question.original_filename)
+  end
+
+  it "has a button to delete the file" do
+    within(page.find("form[action='#{remove_file_url}'][method='post']")) do
+      expect(page).to have_button("Remove")
+    end
+  end
+
+  context "when there is an extra suffix to be added to the heading" do
+    let(:extra_question_text_suffix) { "Some extra text to add to the question text" }
+
+    it "renders the question text and extra suffix as a heading" do
+      expect(page.find("h1")).to have_text("#{question.question_text} #{extra_question_text_suffix}")
+    end
+  end
+
+  context "with unsafe question text" do
+    let(:question_text) { "What is your name? <script>alert(\"Hi\")</script>" }
+    let(:extra_question_text_suffix) { "<span>Some trusted html</span>" }
+
+    it "returns the escaped title with the optional suffix" do
+      expected_output = "What is your name? &lt;script&gt;alert(\"Hi\")&lt;/script&gt; <span>Some trusted html</span>"
+      expect(page.find("h1").native.inner_html).to eq(expected_output)
+    end
+  end
+end
