dab94b19651cf6b8cb2ab8c9040f27f0ac347f94
Jamie Wilkinson <jamie.wilkinson1@digital.cabinet-office.gov.uk>
2025-02-19 11:05:54 +0000

    Delegate methods to ConfirmationDetailsStore
    Pass the `form_id` into the ConfirmationDetailsStore so we can delegate
    the remaining methods that `Context` had wrapper methods for to reduce
    verbosity.
    
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- app/lib/flow/context.rb
+++ app/lib/flow/context.rb
@@ -5,7 +5,7 @@ module Flow
     def initialize(form:, store:)
       @form = form
       @answer_store = Store::SessionAnswerStore.new(store, form.id)
-      @confirmation_details_store = Store::ConfirmationDetailsStore.new(store)
+      @confirmation_details_store = Store::ConfirmationDetailsStore.new(store, form.id)
       @journey = Journey.new(answer_store: @answer_store, form:)
 
       @support_details = OpenStruct.new({
@@ -19,6 +19,7 @@ module Flow
 
     delegate :find_or_create, :previous_step, :next_page_slug, :next_step, :can_visit?, :completed_steps, :all_steps, to: :journey
     delegate :clear_stored_answer, :clear, :form_submitted?, to: :answer_store
+    delegate :save_submission_details, :get_submission_reference, :requested_email_confirmation?, :clear_submission_details, to: :confirmation_details_store
 
     def save_step(step)
       return false unless step.valid?
@@ -26,24 +27,8 @@ module Flow
       step.save_to_store(@answer_store)
     end
 
-    def save_submission_details(reference, requested_email_confirmation)
-      @confirmation_details_store.save_submission_details(form.id, reference, requested_email_confirmation)
-    end
-
-    def get_submission_reference
-      @confirmation_details_store.get_submission_reference(form.id)
-    end
-
-    def requested_email_confirmation?
-      @confirmation_details_store.requested_email_confirmation?(form.id)
-    end
-
-    def clear_submission_details
-      @confirmation_details_store.clear_submission_details(form.id)
-    end
-
   private
 
-    attr_reader :answer_store
+    attr_reader :answer_store, :confirmation_details_store
   end
 end
--- app/lib/store/confirmation_details_store.rb
+++ app/lib/store/confirmation_details_store.rb
@@ -4,27 +4,28 @@ module Store
     SUBMISSION_REFERENCE_KEY = :submission_reference
     REQUESTED_EMAIL_KEY = :requested_email_confirmation
 
-    def initialize(store)
+    def initialize(store, form_id)
       @store = store
+      @form_key = form_id.to_s
       @store[CONFIRMATION_KEY] ||= {}
     end
 
-    def save_submission_details(form_id, reference, requested_email_confirmation)
-      @store[CONFIRMATION_KEY][form_id.to_s] ||= {}
-      @store[CONFIRMATION_KEY][form_id.to_s][SUBMISSION_REFERENCE_KEY.to_s] = reference
-      @store[CONFIRMATION_KEY][form_id.to_s][REQUESTED_EMAIL_KEY.to_s] = requested_email_confirmation
+    def save_submission_details(reference, requested_email_confirmation)
+      @store[CONFIRMATION_KEY][@form_key] ||= {}
+      @store[CONFIRMATION_KEY][@form_key][SUBMISSION_REFERENCE_KEY.to_s] = reference
+      @store[CONFIRMATION_KEY][@form_key][REQUESTED_EMAIL_KEY.to_s] = requested_email_confirmation
     end
 
-    def get_submission_reference(form_id)
-      @store.dig(CONFIRMATION_KEY, form_id.to_s, SUBMISSION_REFERENCE_KEY.to_s)
+    def get_submission_reference
+      @store.dig(CONFIRMATION_KEY, @form_key, SUBMISSION_REFERENCE_KEY.to_s)
     end
 
-    def requested_email_confirmation?(form_id)
-      @store.dig(CONFIRMATION_KEY, form_id.to_s, REQUESTED_EMAIL_KEY.to_s)
+    def requested_email_confirmation?
+      @store.dig(CONFIRMATION_KEY, @form_key, REQUESTED_EMAIL_KEY.to_s)
     end
 
-    def clear_submission_details(form_id)
-      @store[CONFIRMATION_KEY][form_id.to_s] = nil
+    def clear_submission_details
+      @store[CONFIRMATION_KEY][@form_key] = nil
     end
   end
 end
