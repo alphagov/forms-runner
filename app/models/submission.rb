1d073891e5c3ae5413946c619073fcb1612be17d
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-03-03 17:22:27 +0000

    Refactor retreiving Form and Journey for a submission for reuse
    Move logic for retrieving the Form and the Journey for a Submission
    to methods on the Submission. This is so that we can re-use these
    methods in the recurring job to delete submissions and their
    associated files from S3.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,21 @@
 class Submission < ApplicationRecord
+  delegate :preview?, to: :mode_object
+
+  def journey
+    @journey ||= Flow::Journey.new(answer_store:, form:)
+  end
+
+  def form
+    @form ||= Api::V1::FormSnapshotRepository.find_with_mode(id: form_id, mode: mode_object)
+  end
+
+private
+
+  def mode_object
+    Mode.new(mode)
+  end
+
+  def answer_store
+    Store::DatabaseAnswerStore.new(answers)
+  end
 end
