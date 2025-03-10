32e1baa6ba83d36bd7540a559f76dc9d56b75285
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-03-10 12:14:09 +0000

    Add form_document column to submissions table
    In order to construct the email from the answers and forms data when
    the job to send a submission email is run, or when we are attempting
    to resend a bounced submission later on, we need to ensure we are
    using the exact version of the form that existed when the form was
    submitted.
    
    For now, we will store the JSON blob of the form document on the
    submission and use this, rather than retreiving the form from
    forms-api. In the future, we might look at implementing versioning for
    forms so we do not need to store a snapshot of the form document on
    each submission.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_03_163133) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_114758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_03_163133) do
     t.integer "form_id"
     t.jsonb "answers"
     t.string "mode"
+    t.jsonb "form_document"
     t.index ["updated_at"], name: "index_submissions_on_updated_at"
   end
 end
