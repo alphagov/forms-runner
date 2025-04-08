59eddb2681eec2b94ea6a59f0c88618e45e7922e
Jamie Wilkinson <jamie.wilkinson1@digital.cabinet-office.gov.uk>
2025-04-08 15:52:49 +0100

    Add preview_submission_email action mailer preview
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- spec/mailers/aws_ses_form_submission_mailer_preview.rb
+++ spec/mailers/aws_ses_form_submission_mailer_preview.rb
@@ -10,4 +10,16 @@ class AwsSesFormSubmissionMailerPreview < ActionMailer::Preview
                                                                                                          payment_url: nil),
                                                 files: {})
   end
+
+  def preview_submission_email
+    AwsSesFormSubmissionMailer.submission_email(answer_content_html: "<h2>What's your email address?</h2><p>forms@example.gov.uk</p>",
+                                                answer_content_plain_text: "## What's your email address?\n\nforms@example.gov.uk",
+                                                submission_email_address: "testing@gov.uk",
+                                                mailer_options: FormSubmissionService::MailerOptions.new(title: "Form 1",
+                                                                                                         is_preview: true,
+                                                                                                         timestamp: Time.zone.now,
+                                                                                                         submission_reference: Faker::Alphanumeric.alphanumeric(number: 8).upcase,
+                                                                                                         payment_url: nil),
+                                                files: {})
+  end
 end
