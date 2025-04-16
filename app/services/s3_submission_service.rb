f735d73ca861afe4de02a96fc66a8416e93bf132
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-04-16 16:19:23 +0100

    Send uploaded files with S3 submissions
    When the `submission_type` is set to `s3` for a form, and the form has
    answered file upload questions, send the uploaded files to the s3
    bucket set on the form.
    
    Do this by copying the files to the target bucket and then deleting
    them from our S3 bucket. Do not copy the object tagging when we do the
    copy as the form processor does not need the tag added by GuardDuty
    and to copy the tagging we would need to ask form processors to allow
    the s3:PutObjectTagging permission for our IAM role.
    
    Copy the files before uploading the submission S3 to the target bucket
    so that form processors are able to add automations on the CSV file
    being added and be sure that any uploaded files referenced in the CSV
    are already present.
    
    In the tests, stop mocking out the `CsvGenerator` in the tests so we
    can assert on the contents of the file to make sure we're copying
    uploaded file to the submission S3 bucket with the same filename in
    the key as we have in the CSV file

--- app/services/s3_submission_service.rb
+++ app/services/s3_submission_service.rb
@@ -1,5 +1,6 @@
 class S3SubmissionService
-  def initialize(journey:, form:,
+  def initialize(journey:,
+                 form:,
                  timestamp:,
                  submission_reference:,
                  is_preview:)
@@ -8,6 +9,7 @@ class S3SubmissionService
     @timestamp = timestamp
     @submission_reference = submission_reference
     @is_preview = is_preview
+    @file_upload_bucket_name = Settings.aws.file_upload_s3_bucket_name
   end
 
   def submit
@@ -15,38 +17,72 @@ class S3SubmissionService
     raise StandardError, "S3 bucket account ID is not set on the form" if @form.s3_bucket_aws_account_id.nil?
     raise StandardError, "S3 bucket region is not set on the form" if @form.s3_bucket_region.nil?
 
+    # We send the uploaded files before the submissions CSV so that processors can have automations run when the CSV
+    # file arrives and the referenced files will already be present
+    copy_uploaded_files_to_bucket
+
     # rubocop:disable Rails/SaveBang
     Tempfile.create do |file|
       write_submission_csv(file)
-      upload_file_to_s3(file.path)
+      upload_submission_csv_to_s3(file.path)
     end
     # rubocop:enable Rails/SaveBang
+
+    delete_uploaded_files_from_our_bucket
   end
 
 private
 
-  # we only need all_steps. Should we just pass this in, or are we missing something that needs completed_steps?
+  def copy_uploaded_files_to_bucket
+    @journey.completed_file_upload_questions.each(&method(:copy_file_to_bucket))
+  end
+
+  def copy_file_to_bucket(file)
+    source_key = file.uploaded_file_key
+    target_key = uploaded_file_target_key(file)
+    s3_client.copy_object({
+      bucket: @form.s3_bucket_name,
+      expected_bucket_owner: @form.s3_bucket_aws_account_id,
+      copy_source: "/#{@file_upload_bucket_name}/#{source_key}",
+      key: target_key,
+      tagging_directive: "REPLACE", # we don't want to copy tags
+    })
+    Rails.logger.info("Copied uploaded file to submission S3 bucket", {
+      source_key:,
+      target_key:,
+    })
+  end
+
+  def delete_uploaded_files_from_our_bucket
+    @journey.completed_file_upload_questions.each(&:delete_from_s3)
+  end
+
   def write_submission_csv(file)
     CsvGenerator.write_submission(
       all_steps: @journey.all_steps,
       submission_reference: @submission_reference,
       timestamp: @timestamp,
       output_file_path: file.path,
+      is_s3_submission: true,
     )
   end
 
-  def upload_file_to_s3(file_path)
-    s3 = Aws::S3::Client.new(
-      region: @form.s3_bucket_region,
-      credentials: assume_role,
-    )
-    s3.put_object({
+  def upload_submission_csv_to_s3(file_path)
+    key = csv_submission_key
+    s3_client.put_object({
       body: File.read(file_path),
       bucket: @form.s3_bucket_name,
       expected_bucket_owner: @form.s3_bucket_aws_account_id,
-      key: key_name,
+      key: key,
     })
-    Rails.logger.info("Uploaded submission to S3", { key_name: })
+    Rails.logger.info("Uploaded submission to S3", { key: })
+  end
+
+  def s3_client
+    @s3_client ||= Aws::S3::Client.new(
+      region: @form.s3_bucket_region,
+      credentials: assume_role,
+    )
   end
 
   def assume_role
@@ -60,9 +96,17 @@ private
     credentials
   end
 
-  def key_name
+  def csv_submission_key
+    generate_key("form_submission.csv")
+  end
+
+  def uploaded_file_target_key(file)
+    generate_key(file.name_with_filename_suffix)
+  end
+
+  def generate_key(filename)
     folder = @is_preview ? "test_form_submissions" : "form_submissions"
     formatted_timestamp = @timestamp.utc.strftime("%Y%m%dT%H%M%SZ")
-    "#{folder}/#{@form.id}/#{formatted_timestamp}_#{@submission_reference}/form_submission.csv"
+    "#{folder}/#{@form.id}/#{formatted_timestamp}_#{@submission_reference}/#{filename}"
   end
 end
