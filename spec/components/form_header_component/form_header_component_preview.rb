f0136be3e8786891c1e64a9d795494c4b9e4ae1d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-04-28 10:07:01 +0300

    Refactor FormHeaderComponent
    - Rename #service_name_with_tag to product_name_with_tag
    - Rename #service_url to #form_start_page_url
    - Move service name into new private method #form_name
    - Move header classes into new private method #classes
    - Remove unused service_url_overide

--- spec/components/form_header_component/form_header_component_preview.rb
+++ spec/components/form_header_component/form_header_component_preview.rb
@@ -1,25 +1,25 @@
 class FormHeaderComponent::FormHeaderComponentPreview < ViewComponent::Preview
   def default
     mode = Mode.new
-    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
-    render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test", form_slug: "test"))
+    render(FormHeaderComponent::View.new(current_context:, mode:))
   end
 
   def preview_draft
     mode = Mode.new("preview-draft")
-    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
-    render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test", form_slug: "test"))
+    render(FormHeaderComponent::View.new(current_context:, mode:))
   end
 
   def preview_archived
     mode = Mode.new("preview-archived")
-    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
-    render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test", form_slug: "test"))
+    render(FormHeaderComponent::View.new(current_context:, mode:))
   end
 
   def preview_live
     mode = Mode.new("preview-live")
-    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test"), form_slug: "test")
-    render(FormHeaderComponent::View.new(current_context:, mode:, service_url_overide: "/form/1/test"))
+    current_context = OpenStruct.new(form: OpenStruct.new(id: 1, name: "test", form_slug: "test"))
+    render(FormHeaderComponent::View.new(current_context:, mode:))
   end
 end
