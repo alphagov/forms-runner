318e2b51c78b192211f56775cd986765b823376f
Sean Rankine <sean.rankine@me.com>
2025-06-16 18:46:48 +0100

    Move support details  from Flow::Context to Form
    Updated Flow::Context to delegate support details to the Form. This makes it easier to access support details in async jobs where the current context isn't available, but the form is.

--- app/lib/flow/context.rb
+++ app/lib/flow/context.rb
@@ -1,22 +1,15 @@
 module Flow
   class Context
-    attr_reader :form, :support_details, :journey
+    attr_reader :form, :journey
 
     def initialize(form:, store:)
       @form = form
       @answer_store = Store::SessionAnswerStore.new(store, form.id)
       @confirmation_details_store = Store::ConfirmationDetailsStore.new(store, form.id)
       @journey = Journey.new(answer_store: @answer_store, form:)
-
-      @support_details = OpenStruct.new({
-        email: form.support_email,
-        phone: form.support_phone,
-        call_charges_url: "https://www.gov.uk/call-charges",
-        url: form.support_url,
-        url_text: form.support_url_text,
-      })
     end
 
+    delegate :support_details, to: :form
     delegate :find_or_create, :previous_step, :next_page_slug, :next_step, :can_visit?, :completed_steps, :all_steps, to: :journey
     delegate :clear_stored_answer, :clear, :form_submitted?, :answers, to: :answer_store
     delegate :save_submission_details, :get_submission_reference, :requested_email_confirmation?, :clear_submission_details, to: :confirmation_details_store
