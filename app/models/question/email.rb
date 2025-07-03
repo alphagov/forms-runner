60071678e0e71fac66ae9e1d502d57ff5b24b240
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-07-03 14:49:52 +0100

    Fully validate email format for email questions
    Previously, we were only validating that the email contained an @
    symbol somewhere in it. We now have proper email validation, which we
    use for validation the email address entered to receive a confirmation
    email. Use this validation for email questions.

--- app/models/question/email.rb
+++ app/models/question/email.rb
@@ -1,13 +1,7 @@
 module Question
   class Email < Question::QuestionBase
-    # see https://design-system.service.gov.uk/patterns/email-addresses/
-    # This model contains very lite checking - only that an @ exists. Pay and
-    # notify both have good examples of better email validation checks and ways
-    # to help users enter the right value
-
-    EMAIL_REGEX = /.*@.*/
     attribute :email
     validates :email, presence: true, unless: :is_optional?
-    validates :email, format: { with: EMAIL_REGEX, message: :invalid_email }, allow_blank: true
+    validates :email, email_address: { message: :invalid_email }, allow_blank: true
   end
 end
--- spec/models/question/email_spec.rb
+++ spec/models/question/email_spec.rb
@@ -10,52 +10,60 @@ RSpec.describe Question::Email, type: :model do
 
   it_behaves_like "a question model"
 
-  context "when given an empty string or nil" do
-    it "returns invalid with blank email" do
-      expect(question).not_to be_valid
-      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
+  shared_examples "format validations" do
+    context "when given a valid email address" do
+      let(:email) { Faker::Internet.email }
+
+      before do
+        question.email = email
+      end
+
+      it "is valid" do
+        expect(question).to be_valid
+      end
+
+      it "is included in show_answer" do
+        expect(question.show_answer).to eq email
+      end
+
+      it "returns the email address in show_answer_in_csv" do
+        expect(question.show_answer_in_csv).to eq(Hash[question_text, email])
+      end
     end
 
-    it "returns invalid with empty string" do
-      question.email = ""
-      expect(question).not_to be_valid
-      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
-      expect(question.errors[:email]).not_to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
-    end
-
-    it "shows as a blank string" do
-      expect(question.show_answer).to eq ""
-    end
-
-    it "returns a hash with an blank value for show_answer_in_csv" do
-      expect(question.show_answer_in_csv).to eq(Hash[question_text, ""])
+    context "when given an invalid email address" do
+      it "is invalid" do
+        question.email = "no-tld@domain"
+        expect(question).not_to be_valid
+        expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
+      end
     end
   end
 
-  context "when given a string with an @ symbol in" do
-    before do
-      question.email = " @ "
+  context "when question is mandatory" do
+    context "when given an empty string or nil" do
+      it "returns invalid with blank email" do
+        expect(question).not_to be_valid
+        expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
+      end
+
+      it "returns invalid with empty string" do
+        question.email = ""
+        expect(question).not_to be_valid
+        expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.blank"))
+        expect(question.errors[:email]).not_to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
+      end
+
+      it "shows as a blank string" do
+        expect(question.show_answer).to eq ""
+      end
+
+      it "returns a hash with an blank value for show_answer_in_csv" do
+        expect(question.show_answer_in_csv).to eq(Hash[question_text, ""])
+      end
     end
 
-    it "validates" do
-      expect(question).to be_valid
-    end
-
-    it "is included in show_answer" do
-      expect(question.show_answer).to eq " @ "
-    end
-
-    it "returns the email address in show_answer_in_csv" do
-      expect(question.show_answer_in_csv).to eq(Hash[question_text, " @ "])
-    end
-  end
-
-  context "when given a string without an @ symbol in" do
-    it "does not validate an address without an @" do
-      question.email = "not an email address"
-      expect(question).not_to be_valid
-      expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
-    end
+    include_examples "format validations"
   end
 
   context "when question is optional" do
@@ -81,30 +89,6 @@ RSpec.describe Question::Email, type: :model do
       expect(question.show_answer_in_csv).to eq(Hash[question_text, ""])
     end
 
-    context "when given a string with an @ symbol in" do
-      before do
-        question.email = " @ "
-      end
-
-      it "validates" do
-        expect(question).to be_valid
-      end
-
-      it "is included in show_answer" do
-        expect(question.show_answer).to eq " @ "
-      end
-
-      it "returns the email address in show_answer_in_csv" do
-        expect(question.show_answer_in_csv).to eq(Hash[question_text, " @ "])
-      end
-    end
-
-    context "when given a string without an @ symbol in" do
-      it "does not validate an address without an @" do
-        question.email = "not an email address"
-        expect(question).not_to be_valid
-        expect(question.errors[:email]).to include(I18n.t("activemodel.errors.models.question/email.attributes.email.invalid_email"))
-      end
-    end
+    include_examples "format validations"
   end
 end
