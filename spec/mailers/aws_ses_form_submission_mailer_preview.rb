ff2de0ced60d47dd5af6db1d80c04ca3877b0d52
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-10-16 14:31:46 +0100

    Update mailer previews
    Update mailer previews to reflect the changes to refactor the
    AwsSesFormSubmissionMailer to use the Submission model.

--- spec/mailers/aws_ses_form_submission_mailer_preview.rb
+++ spec/mailers/aws_ses_form_submission_mailer_preview.rb
@@ -1,49 +1,39 @@
 class AwsSesFormSubmissionMailerPreview < ActionMailer::Preview
+  include FactoryBot::Syntax::Methods
+
   def submission_email
+    form_document = build(:v2_form_document, submission_email: "testing@gov.uk")
+    submission = build(:submission, form_document:, is_preview: false)
     AwsSesFormSubmissionMailer.submission_email(answer_content_html: "<h3>What's your email address?</h3><p>forms@example.gov.uk</p>",
                                                 answer_content_plain_text: "What's your email address?\n\nforms@example.gov.uk",
-                                                submission_email_address: "testing@gov.uk",
-                                                mailer_options: FormSubmissionService::MailerOptions.new(title: "Form 1",
-                                                                                                         is_preview: false,
-                                                                                                         timestamp: Time.zone.now,
-                                                                                                         submission_reference: Faker::Alphanumeric.alphanumeric(number: 8).upcase,
-                                                                                                         payment_url: nil),
+                                                submission:,
                                                 files: {})
   end
 
   def preview_submission_email
+    form_document = build(:v2_form_document, submission_email: "testing@gov.uk")
+    submission = build(:submission, form_document:, is_preview: true)
     AwsSesFormSubmissionMailer.submission_email(answer_content_html: "<h3>What's your email address?</h3><p>forms@example.gov.uk</p>",
                                                 answer_content_plain_text: "What's your email address?\n\nforms@example.gov.uk",
-                                                submission_email_address: "testing@gov.uk",
-                                                mailer_options: FormSubmissionService::MailerOptions.new(title: "Form 1",
-                                                                                                         is_preview: true,
-                                                                                                         timestamp: Time.zone.now,
-                                                                                                         submission_reference: Faker::Alphanumeric.alphanumeric(number: 8).upcase,
-                                                                                                         payment_url: nil),
+                                                submission:,
                                                 files: {})
   end
 
   def submission_email_with_payment_link
+    form_document = build(:v2_form_document, submission_email: "testing@gov.uk", payment_url: "https://www.gov.uk/payments/your-payment-link")
+    submission = build(:submission, form_document:, is_preview: false)
     AwsSesFormSubmissionMailer.submission_email(answer_content_html: "<h3>What's your email address?</h3><p>forms@example.gov.uk</p>",
                                                 answer_content_plain_text: "What's your email address?\n\nforms@example.gov.uk",
-                                                submission_email_address: "testing@gov.uk",
-                                                mailer_options: FormSubmissionService::MailerOptions.new(title: "Form 1",
-                                                                                                         is_preview: true,
-                                                                                                         timestamp: Time.zone.now,
-                                                                                                         submission_reference: Faker::Alphanumeric.alphanumeric(number: 8).upcase,
-                                                                                                         payment_url: "https://www.gov.uk/payments/your-payment-link"),
+                                                submission:,
                                                 files: {})
   end
 
   def submission_email_with_csv
+    form_document = build(:v2_form_document, submission_email: "testing@gov.uk", payment_url: "https://www.gov.uk/payments/your-payment-link")
+    submission = build(:submission, form_document:, is_preview: false)
     AwsSesFormSubmissionMailer.submission_email(answer_content_html: "<h3>What's your email address?</h3><p>forms@example.gov.uk</p>",
                                                 answer_content_plain_text: "What's your email address?\n\nforms@example.gov.uk",
-                                                submission_email_address: "testing@gov.uk",
-                                                mailer_options: FormSubmissionService::MailerOptions.new(title: "Form 1",
-                                                                                                         is_preview: true,
-                                                                                                         timestamp: Time.zone.now,
-                                                                                                         submission_reference: Faker::Alphanumeric.alphanumeric(number: 8).upcase,
-                                                                                                         payment_url: "https://www.gov.uk/payments/your-payment-link"),
+                                                submission:,
                                                 files: {},
                                                 csv_filename: "my_answers.csv")
   end
