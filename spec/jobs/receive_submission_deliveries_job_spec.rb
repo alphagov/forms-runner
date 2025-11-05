978969f93ca3282250559b03db5222c05f3197bc
Sean Rankine <sean.rankine@me.com>
2025-11-05 14:59:09 +0000

    Refactor tests for SES notifications jobs
    This removes assertions that are now covered by the AwsSesMessagePoller
    tests.

--- spec/jobs/receive_submission_deliveries_job_spec.rb
+++ spec/jobs/receive_submission_deliveries_job_spec.rb
@@ -39,189 +39,90 @@ RSpec.describe ReceiveSubmissionDeliveriesJob, type: :job do
     end
   end
 
-  context "when there are messages in the queue" do
+  before do
+    sts_client = instance_double(Aws::STS::Client)
+    allow(Aws::STS::Client).to receive(:new).and_return(sts_client)
+    allow(sts_client).to receive(:get_caller_identity).and_return(OpenStruct.new(account: "123456789012"))
+
+    allow(Aws::SQS::Client).to receive(:new).and_return(sqs_client)
+    allow(sqs_client).to receive(:receive_message).and_return(OpenStruct.new(messages: messages), OpenStruct.new(messages: []))
+    allow(sqs_client).to receive(:delete_message)
+
+    allow(CloudWatchService).to receive(:record_job_started_metric)
+
+    Rails.logger.broadcast_to logger
+  end
+
+  after do
+    Rails.logger.stop_broadcasting_to logger
+  end
+
+  describe "CloudWatch metrics" do
+    let(:messages) { [sqs_message] }
+
     before do
-      sts_client = instance_double(Aws::STS::Client)
-      allow(Aws::STS::Client).to receive(:new).and_return(sts_client)
-      allow(sts_client).to receive(:get_caller_identity).and_return(OpenStruct.new(account: "123456789012"))
-
-      allow(Aws::SQS::Client).to receive(:new).and_return(sqs_client)
-      allow(sqs_client).to receive(:receive_message).and_return(OpenStruct.new(messages: messages), OpenStruct.new(messages: []))
-      allow(sqs_client).to receive(:delete_message) do
-        messages.shift
-      end
-
-      allow(CloudWatchService).to receive(:record_job_started_metric)
-
-      Rails.logger.broadcast_to logger
+      allow(CloudWatchService).to receive(:record_submission_delivery_latency_metric)
     end
 
-    after do
-      Rails.logger.stop_broadcasting_to logger
+    it "sends job started metric" do
+      described_class.perform_now
+      expect(CloudWatchService).to have_received(:record_job_started_metric).with("ReceiveSubmissionDeliveriesJob")
     end
 
-    context "when there are many messages in the queue" do
-      let(:message_count) { 15 }
-      let(:messages) { Array.new(message_count, sqs_message) }
+    it "sends submission delivery latency metric" do
+      described_class.perform_now
+      # latency is ses_delivery_timestamp - submission.created_at
+      expect(CloudWatchService).to have_received(:record_submission_delivery_latency_metric).with(6122, "Email")
+    end
+  end
 
-      before do
-        allow(sqs_client).to receive(:receive_message).and_return(OpenStruct.new(messages: messages[0..9]), OpenStruct.new(messages: messages[10..15]), OpenStruct.new(messages: []))
+  describe "processing delivery notifications" do
+    let(:messages) { [sqs_message] }
 
-        described_class.perform_later
-      end
-
-      it "clears the SQS queue" do
-        perform_enqueued_jobs
-        expect(sqs_client).to have_received(:delete_message).exactly(message_count).times
-      end
-
-      it "sends cloudwatch metric" do
-        perform_enqueued_jobs
-        expect(CloudWatchService).to have_received(:record_job_started_metric).with("ReceiveSubmissionDeliveriesJob")
-      end
+    before do
+      job = described_class.perform_later
+      @job_id = job.job_id
+      allow(CloudWatchService).to receive(:record_submission_delivery_latency_metric)
     end
 
-    context "when the message is a delivery notification" do
-      let(:messages) { [sqs_message] }
-
-      before do
-        job = described_class.perform_later
-        @job_id = job.job_id
-        allow(CloudWatchService).to receive(:record_submission_delivery_latency_metric)
-      end
-
-      it "doesn't change the mail status for other submissions" do
-        perform_enqueued_jobs
-        expect(other_submission.reload.bounced?).to be true
-      end
-
-      it "logs at info level" do
-        perform_enqueued_jobs
-
-        expect(log_lines).to include(hash_including(
-                                       "level" => "INFO",
-                                       "message" => "Form event",
-                                       "event" => "form_submission_delivered",
-                                       "form_id" => form_with_file_upload.form_id,
-                                       "submission_reference" => reference,
-                                       "preview" => "false",
-                                       "sns_message_timestamp" => sns_message_timestamp,
-                                       "job_id" => @job_id,
-                                       "job_class" => "ReceiveSubmissionDeliveriesJob",
-                                     ))
-      end
-
-      it "saves the submission delivery time to the submission record" do
-        perform_enqueued_jobs
-        expect(submission.reload.delivered_at).to eq ses_delivery_timestamp
-      end
-
-      it "sends cloudwatch metric for submission delivery time" do
-        perform_enqueued_jobs
-        # latency is ses_delivery_timestamp - submission.created_at
-        expect(CloudWatchService).to have_received(:record_submission_delivery_latency_metric).with(6122, "Email")
-      end
-
-      it "deletes the SQS message" do
-        perform_enqueued_jobs
-        expect(sqs_client).to have_received(:delete_message).with(queue_url: anything, receipt_handle:)
-      end
-
-      context "when there is no submission found" do
-        let(:ses_delivery_message_body) { { "mail" => { "messageId" => "mismatched-message-id" }, "eventType": "Delivery" } }
-
-        it "sends an error to Sentry" do
-          allow(Sentry).to receive(:capture_exception)
-
-          perform_enqueued_jobs
-
-          expect(Sentry).to have_received(:capture_exception) do |error|
-            expect(error.class).to eq(ActiveRecord::RecordNotFound)
-          end
-        end
-
-        it "does not delete the SQS message" do
-          perform_enqueued_jobs
-
-          expect(sqs_client).not_to have_received(:delete_message)
-        end
-      end
-
-      context "when updating submission fails" do
-        let(:messages) { [sqs_message, sqs_message] }
-
-        before do
-          call_count = 0
-
-          allow(Submission).to receive(:find_by) do
-            call_count += 1
-            if call_count == 1
-              raise StandardError, "Test error"
-            else
-              submission
-            end
-          end
-        end
-
-        it "logs at warn level" do
-          begin
-            perform_enqueued_jobs
-          rescue StandardError
-            nil
-          end
-
-          expect(log_lines).to include(hash_including(
-                                         "level" => "WARN",
-                                         "mail_message_id" => mail_message_id,
-                                         "sqs_message_id" => sqs_message_id,
-                                         "sns_message_timestamp" => sns_message_timestamp,
-                                         "message" => "Error processing message - StandardError: Test error",
-                                         "job_id" => @job_id,
-                                         "job_class" => "ReceiveSubmissionDeliveriesJob",
-                                       ))
-        end
-
-        it "sends an error to Sentry" do
-          allow(Sentry).to receive(:capture_exception)
-
-          perform_enqueued_jobs
-
-          expect(Sentry).to have_received(:capture_exception)
-        end
-
-        it "continues processing subsequent messages" do
-          expected_loop_count = messages.length - 1
-
-          perform_enqueued_jobs
-
-          expect(sqs_client).to have_received(:delete_message).exactly(expected_loop_count).times
-        end
-      end
+    it "updates the submission delivered_at timestamp" do
+      perform_enqueued_jobs
+      expect(submission.reload.delivered_at).to eq ses_delivery_timestamp
     end
 
-    context "when the message is not a delivery notification" do
-      let(:event_type) { "Some other event type" }
-      let(:messages) { [sqs_message] }
+    it "doesn't change the delivery status for other submissions" do
+      perform_enqueued_jobs
+      expect(other_submission.reload.bounced?).to be true
+    end
 
-      before do
-        job = described_class.perform_later
-        @job_id = job.job_id
-      end
+    it "logs form event with correct details" do
+      perform_enqueued_jobs
 
-      it "sends an error to Sentry" do
-        allow(Sentry).to receive(:capture_exception)
+      expect(log_lines).to include(hash_including(
+                                     "level" => "INFO",
+                                     "message" => "Form event",
+                                     "event" => "form_submission_delivered",
+                                     "form_id" => form_with_file_upload.form_id,
+                                     "submission_reference" => reference,
+                                     "preview" => "false",
+                                     "sns_message_timestamp" => sns_message_timestamp,
+                                     "job_id" => @job_id,
+                                     "job_class" => "ReceiveSubmissionDeliveriesJob",
+                                   ))
+    end
+  end
 
-        perform_enqueued_jobs
+  describe "handling unexpected event types" do
+    let(:event_type) { "Some other event type" }
+    let(:messages) { [sqs_message] }
 
-        expect(Sentry).to have_received(:capture_exception) do |error|
-          expect(error.message).to eq("Unexpected event type:#{event_type}")
-        end
-      end
+    it "raises an error with the unexpected event type" do
+      allow(Sentry).to receive(:capture_exception)
 
-      it "does not delete the SQS message" do
-        perform_enqueued_jobs
+      described_class.perform_now
 
-        expect(sqs_client).not_to have_received(:delete_message)
+      expect(Sentry).to have_received(:capture_exception) do |error|
+        expect(error.message).to eq("Unexpected event type:#{event_type}")
       end
     end
   end
