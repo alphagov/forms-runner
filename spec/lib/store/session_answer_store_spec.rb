489eb9eff17943146eed7f2807dfdb4b8790338e
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-11-28 14:47:41 +0000

    Revert "Retreive answers from store using external ID or database ID"
    This reverts commit 907ca881d33b80db94bdadc73e1c3fd4d0d839dc.
    
    All sessions should now use the external ID to store answers, so we no
    longer need the fallback to using the database ID.

--- spec/lib/store/session_answer_store_spec.rb
+++ spec/lib/store/session_answer_store_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe Store::SessionAnswerStore do
 
   let(:store) { {} }
   let(:form_id) { 1 }
-  let(:step_id) { 5 }
-  let(:database_id) { nil }
-  let(:step) { instance_double(Step, { page_id: step_id, database_id: }) }
+  let(:step) { OpenStruct.new({ page_id: "5" }) }
   let(:other_step) { OpenStruct.new({ page_id: "1" }) }
   let(:reference) { Faker::Alphanumeric.alphanumeric(number: 8).upcase }
   let(:requested_email_confirmation) { true }
@@ -30,65 +28,19 @@ RSpec.describe Store::SessionAnswerStore do
   end
 
   describe "#clear_stored_answer" do
-    context "when the answer was stored using the step id as the key" do
-      before do
-        answer_store.save_step(step, "test answer")
-      end
-
-      it "clears a single answer for a step" do
-        expect(answer_store.get_stored_answer(step)).to eq("test answer")
-        answer_store.clear_stored_answer(step)
-        expect(answer_store.get_stored_answer(step)).to be_nil
-      end
-
-      it "does not error if removing a step which doesn't exist in the store" do
-        answer_store.clear_stored_answer(other_step)
-        expect(answer_store.get_stored_answer(step)).to eq("test answer")
-      end
+    before do
+      answer_store.save_step(step, "test answer")
     end
 
-    context "when the the answer was stored using the database_id as the key" do
-      let(:step_id) { "abcABC12" }
-      let(:database_id) { 123 }
-      let(:answer) { "test answer" }
-      let(:store) do
-        {
-          answers: {
-            form_id.to_s => {
-              database_id.to_s => answer,
-            },
-          },
-        }
-      end
-
-      it "clears a single answer for a step" do
-        expect(answer_store.get_stored_answer(step)).to eq("test answer")
-        answer_store.clear_stored_answer(step)
-        expect(answer_store.get_stored_answer(step)).to be_nil
-      end
+    it "clears a single answer for a step" do
+      expect(answer_store.get_stored_answer(step)).to eq("test answer")
+      answer_store.clear_stored_answer(step)
+      expect(answer_store.get_stored_answer(step)).to be_nil
     end
 
-    context "when there are answers stored using both the id and database_id of the step" do
-      let(:step_id) { "abcABC12" }
-      let(:database_id) { 123 }
-      let(:answer) { "test answer" }
-
-      let(:store) do
-        {
-          answers: {
-            form_id.to_s => {
-              database_id.to_s => "an answer we will ignore",
-              step_id => answer,
-            },
-          },
-        }
-      end
-
-      it "clears both answers" do
-        expect(answer_store.get_stored_answer(step)).to eq("test answer")
-        answer_store.clear_stored_answer(step)
-        expect(answer_store.answers).to eq({})
-      end
+    it "does not error if removing a step which doesn't exist in the store" do
+      answer_store.clear_stored_answer(other_step)
+      expect(answer_store.get_stored_answer(step)).to eq("test answer")
     end
   end
 
@@ -114,71 +66,17 @@ RSpec.describe Store::SessionAnswerStore do
   end
 
   describe "#get_stored_answer" do
-    context "when the answer was stored using the step id as the key" do
-      before do
-        answer_store.save_step(step, "test answer")
-        answer_store.save_step(other_step, "test2 answer")
-      end
-
-      it "returns the answer for the first step" do
-        expect(answer_store.get_stored_answer(step)).to eq("test answer")
-      end
-
-      it "returns the answer for the second step" do
-        expect(answer_store.get_stored_answer(other_step)).to eq("test2 answer")
-      end
+    before do
+      answer_store.save_step(step, "test answer")
+      answer_store.save_step(other_step, "test2 answer")
     end
 
-    context "when the answer was stored using the database_id as the key" do
-      let(:step_id) { "abcABC12" }
-      let(:step_answer_store_key) { 123 }
-      let(:answer) { "test answer" }
-      let(:store) do
-        {
-          answers: {
-            form_id.to_s => {
-              step_answer_store_key.to_s => answer,
-            },
-          },
-        }
-      end
-
-      context "when the step has a database_id" do
-        let(:database_id) { step_answer_store_key }
-
-        it "gets the answer by the database ID" do
-          expect(answer_store.get_stored_answer(step)).to eq(answer)
-        end
-      end
-
-      context "when the step does not have a database_id" do
-        let(:database_id) { nil }
-
-        it "returns nil" do
-          expect(answer_store.get_stored_answer(step)).to be_nil
-        end
-      end
+    it "returns the answer for the first step" do
+      expect(answer_store.get_stored_answer(step)).to eq("test answer")
     end
 
-    context "when there are answers stored using both the id and database_id of the step" do
-      let(:step_id) { "abcABC12" }
-      let(:database_id) { 123 }
-      let(:answer) { "test answer" }
-
-      let(:store) do
-        {
-          answers: {
-            form_id.to_s => {
-              database_id.to_s => "an answer we will ignore",
-              step_id => answer,
-            },
-          },
-        }
-      end
-
-      it "returns the answer stored using the step id" do
-        expect(answer_store.get_stored_answer(step)).to eq(answer)
-      end
+    it "returns the answer for the second step" do
+      expect(answer_store.get_stored_answer(other_step)).to eq("test2 answer")
     end
   end
 
