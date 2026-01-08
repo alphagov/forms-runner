9fa99be2bcbc1d5536ea4abe09eb75c5a9630020
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-01-08 14:30:29 +0000

    Fix component previews
    The preview functionality in the ViewCOmponent library has changed its
    configuration API since we initially configured it, breaking all of our
    component preview links.
    
    This commit updates to the new configuration, and adds a test so we'll
    be alerted if config changes break the previews again in future.

--- spec/features/components/preview_components_spec.rb
+++ spec/features/components/preview_components_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe "Previewing components", type: :feature do
   it "checks all component previews for axe errors" do
     visit "/preview/"
+    expect(page).to have_text("Preview")
     links = page.all("li > a").map { |a| a["href"] }
     links.each do |link|
       visit link
@@ -12,6 +13,7 @@ describe "Previewing components", type: :feature do
 
   it "checks GDS Transport font is available" do
     visit "/preview/"
+    expect(page).to have_text("Preview")
     font_loaded = evaluate_script("document.fonts.check('12px GDS Transport')")
 
     expect(font_loaded).to be true
