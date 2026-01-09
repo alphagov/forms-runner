a8a3d08cde6603c5bbb1e44d91be7c963801480f
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2026-01-09 16:06:45 +0000

    configure with envars

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,26 +1,12 @@
 require "opentelemetry/sdk"
 require "opentelemetry/instrumentation/all"
 
-return unless defined?(Rails::Server) || ENV["ENABLE_OTEL"] == "true"
+return unless ENV["ENABLE_OTEL"] == "true"
 
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = "forms-runner"
+  instrumentation_config = { "OpenTelemetry::Instrumentation::Rack" => { untraced_endpoints: ["/up"] } }
+  c.use_all(instrumentation_config)
 
-  config = { "OpenTelemetry::Instrumentation::Rack" => { untraced_endpoints: ["/up"] } }
-
-  c.add_span_processor(
-    # Use the BatchSpanProcessor to send traces in groups instead of one at a time
-    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-      # Use the default OLTP Exporter to send traces to the ADOT Collector
-      OpenTelemetry::Exporter::OTLP::Exporter.new(
-        # The OpenTelemetry Collector is running as a sidecar and listening on port 4318
-        endpoint: "http://127.0.0.1:4318/v1/traces",
-      ),
-    ),
-  )
-
-  # The X-Ray Propagator injects the X-Ray Tracing Header into downstream calls
-  c.propagators = [OpenTelemetry::Propagator::XRay::TextMapPropagator.new]
-
-  c.use_all(config)
+  # Disable logging for Rake tasks to avoid cluttering output
+  c.logger = Logger.new(File::NULL) if Rails.const_defined?(:Rake) && Rake.application.top_level_tasks.any?
 end
