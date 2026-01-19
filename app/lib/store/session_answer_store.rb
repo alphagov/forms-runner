f3f6088eeafb291ac540197f28006fd4143e7da3
Thomas Iles <tomiles@gmail.com>
2026-01-19 15:37:02 +0000

    Add locales to session answer store
    We are going to store the locales used when filling in a form in the session answer store.
    
    They will be stored in an array under the key `locales`, form_id, in the store.
    
    We clear the locales when the form is submitted at the same time we
    clear the answers.

--- app/lib/store/session_answer_store.rb
+++ app/lib/store/session_answer_store.rb
@@ -3,11 +3,13 @@ module Store
     include Store::Access
 
     ANSWERS_KEY = :answers
+    LOCALES_KEY = :locales
 
     def initialize(store, form_id)
       @store = store
       @form_key = form_id.to_s
       @store[ANSWERS_KEY] ||= {}
+      @store[LOCALES_KEY] ||= {}
     end
 
     def save_step(step, answer)
@@ -25,6 +27,7 @@ module Store
 
     def clear
       @store[ANSWERS_KEY][@form_key] = nil
+      @store[LOCALES_KEY][@form_key] = nil
     end
 
     def form_submitted?
@@ -33,6 +36,16 @@ module Store
 
     def answers
       @store[ANSWERS_KEY][@form_key]
+    end
+
+    def add_locale(locale)
+      @store[LOCALES_KEY][@form_key] ||= []
+      @store[LOCALES_KEY][@form_key] |= [locale.to_s]
+    end
+
+    def locales_used
+      locales = @store.dig(LOCALES_KEY, @form_key) || []
+      locales.map(&:to_sym) || []
     end
   end
 end
