428c6e59a7bd3e0f07659a0690e9d639297da14f
Thomas Iles <tomiles@gmail.com>
2026-01-28 10:38:43 +0000

    Change JSON generator to use language
    The JSON generator used to take submission_locale as an argument, and
    always include it in the JSON.
    
    Now, only submissions for forms with Welsh will have language set
    to en or cy.
    
    This means all current forms won't include a language field unless they
    have a Welsh translation added. When the JSON is generated the language
    field will be set to nil and the field won't be included.
    
    The argument name has been changed to language as it's more direct and
    shows what's being set in the JSON.

--- app/lib/json_submission_generator.rb
+++ app/lib/json_submission_generator.rb
@@ -1,13 +1,17 @@
 class JsonSubmissionGenerator
-  def self.generate_submission(form:, all_steps:, submission_reference:, timestamp:, is_s3_submission:, submission_locale:)
+  def self.generate_submission(form:, all_steps:, submission_reference:, timestamp:, is_s3_submission:, language:)
     submission = {
       "$schema" => "#{Settings.forms_product_page.base_url}/json-submissions/v1/schema",
       form_name: form.name,
       submission_reference:,
       submitted_at: timestamp.getutc.iso8601(3),
-      language: submission_locale,
       answers: all_steps.flat_map { |step| step.show_answer_in_json(is_s3_submission) },
     }
+
+    if language.present?
+      submission[:language] = language
+    end
+
     JSON.pretty_generate(submission)
   end
 end
