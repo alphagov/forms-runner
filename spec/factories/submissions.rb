e13fc4b1b25a0068689fc8e6b8836cfb54a2ec55
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-02-05 17:17:02 +0000

    Remove unncessary attributes from submission factory
    These attributes are no longer used, as we are now using deliveries
    to track the delivery status of a submission.

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :submission do
     created_at { Time.zone.now - 2.minutes }
     updated_at { created_at }
-    last_delivery_attempt { nil }
     reference { Faker::Alphanumeric.alphanumeric(number: 8).upcase }
     form_id { Faker::Number.number(digits: 5) }
     answers do
@@ -17,7 +16,6 @@ FactoryBot.define do
     end
     mode { is_preview ? "preview-live" : "live" }
     form_document { build :v2_form_document, form_id: }
-    delivery_status { :pending }
     submission_locale { :en }
 
     transient do
@@ -25,9 +23,6 @@ FactoryBot.define do
     end
 
     trait :sent do
-      mail_message_id { Faker::Alphanumeric.alphanumeric }
-      last_delivery_attempt { created_at + 1.minute }
-
       transient do
         delivery_reference { Faker::Alphanumeric.alphanumeric }
       end
