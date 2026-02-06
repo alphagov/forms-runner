f2d3fd04637cc34939690c259bf9f7d0c94f989c
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-02-06 13:00:07 +0000

    Send metric for failed solid queue job executions count
    Send a metric to CloudWatch for the count of failed Solid queue job
    executions. This will allow us to set up an alarm to notify us when
    there are failed executions that need to be investigated and manually
    retried.
    
    Include the queue name as a dimension on the metric so we can set up
    alarms for failed jobs on specific queues.
    
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

--- app/jobs/queue_metrics_job.rb
+++ app/jobs/queue_metrics_job.rb
@@ -6,5 +6,11 @@ class QueueMetricsJob < ApplicationJob
   def perform(*_args)
     submissions_queue_length = SolidQueue::Queue.find_by_name(SUBMISSIONS_QUEUE_NAME).size
     CloudWatchService.record_queue_length_metric(SUBMISSIONS_QUEUE_NAME, submissions_queue_length)
+
+    SolidQueue::FailedExecution.joins(:job)
+                               .group_by { |failed_execution| failed_execution.job.queue_name }
+                               .each do |queue_name, failed_executions|
+      CloudWatchService.record_failed_job_executions(queue_name, failed_executions.count)
+    end
   end
 end
