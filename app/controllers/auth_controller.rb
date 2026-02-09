5c35aedc1e8bd3f25951f120b8bf54e3c1d0fc82
Thomas Iles <tomiles@gmail.com>
2026-02-09 16:49:01 +0000

    Add new radio button option

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,11 @@ class AuthController < ApplicationController
     # We come here after one login
     # We will need to restore, form_id, mode and form_slug, which we should have saved before sending
     if session["return_to"].present?
+
+      # We need to do something to get the user's email here.
+      #
+      # We then need to set the email in the session somewhere
+      session["one_login_email"] = "example@example.org"
       redirect_to session["return_to"]
     end
   end
