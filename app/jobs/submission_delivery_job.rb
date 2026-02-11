a0f305e740c2c1d3831e0fef1a0f89283f73bdc4
Sean Rankine <sean.rankine@me.com>
2026-02-11 12:40:11 +0000

    Add SendS3SubmissionJob
    This creates an async job the can deliver Submissions to S3.

--- app/jobs/submission_delivery_job.rb
+++ app/jobs/submission_delivery_job.rb
@@ -20,7 +20,7 @@ private
 
   def record_submission_sent!
     milliseconds_since_scheduled = (Time.current - scheduled_at_or_enqueued_at).in_milliseconds.round
-    EventLogger.log_form_event("submission_email_sent", { milliseconds_since_scheduled: })
+    EventLogger.log_form_event("submission_sent", { milliseconds_since_scheduled: })
     CloudWatchService.record_submission_sent_metric(milliseconds_since_scheduled)
   end
 
