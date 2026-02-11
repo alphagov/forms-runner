19bc573de3d3c7f1d0bc096337c5c502b5bc6424
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2026-02-11 10:28:29 +0000

    Add batch_frequencies to Deliveries
    This will allow us to identify whether a delivery is for a batch of
    submissions or an individual submission, and what the time period the
    batch covers should be.
    
    Currently this will either be nil for a single submission delivery,
    or "daily" for a daily batch.
    
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_06_111513) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_11_102457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "deliveries", force: :cascade do |t|
+    t.string "batch_frequency"
     t.datetime "created_at", null: false
     t.datetime "delivered_at"
     t.string "delivery_reference"
