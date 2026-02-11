139599122d615fd47f22bd682769e0997c193562
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2026-02-11 12:09:18 +0000

    Add batch_csv_filename to filename generator
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- spec/lib/submission_filename_generator_spec.rb
+++ spec/lib/submission_filename_generator_spec.rb
@@ -44,4 +44,57 @@ RSpec.describe SubmissionFilenameGenerator do
       end
     end
   end
+
+  describe "#batch_csv_filename" do
+    let(:date) { Time.zone.local(2024, 6, 1) }
+    let(:mode) { instance_double(Mode, preview?: false) }
+    let(:form_version) { "1" }
+
+    context "when there is a long form name that would cause the filename to be longer than 100 characters" do
+      let(:form_name) { "A form name that will cause the filename to be truncated to obey the limit" }
+
+      it "truncates the form name in the filename" do
+        filename = described_class.batch_csv_filename(form_name:, date:, mode:, form_version:)
+        expect(filename).to eq("govuk_forms_a_form_name_that_will_cause_the_filename_to_be_truncated_to_obey_the_2024-06-01_1.csv")
+      end
+    end
+
+    context "when the form name would cause the filename to be exactly 100 characters long" do
+      let(:form_name) { "Form name that will cause the filename to be 100 characters long exact" }
+
+      it "does not truncate the form name in the filename" do
+        filename = described_class.batch_csv_filename(form_name:, date:, mode:, form_version:)
+        expect(filename).to eq("govuk_forms_form_name_that_will_cause_the_filename_to_be_100_characters_long_exact_2024-06-01_1.csv")
+      end
+    end
+
+    context "when there is no form version provided" do
+      let(:form_name) { "Form name" }
+      let(:form_version) { nil }
+
+      it "does not include the form version in the filename" do
+        filename = described_class.batch_csv_filename(form_name:, date:, mode:, form_version:)
+        expect(filename).to eq("govuk_forms_form_name_2024-06-01.csv")
+      end
+    end
+
+    context "when the mode is preview" do
+      let(:mode) { instance_double(Mode, preview?: true) }
+      let(:form_name) { "Form name" }
+
+      it "includes 'test_' in the filename" do
+        filename = described_class.batch_csv_filename(form_name:, date:, mode:, form_version:)
+        expect(filename).to eq("test_govuk_forms_form_name_2024-06-01_1.csv")
+      end
+
+      context "when the form name would cause the filename to be over 100 characters long including the 'test_' prefix" do
+        let(:form_name) { "A form name that will cause the filename to be truncated to obey the limit" }
+
+        it "truncates the form name in the filename" do
+          filename = described_class.batch_csv_filename(form_name:, date:, mode:, form_version:)
+          expect(filename).to eq("test_govuk_forms_a_form_name_that_will_cause_the_filename_to_be_truncated_to_obey_2024-06-01_1.csv")
+        end
+      end
+    end
+  end
 end
