44823ff69e319a731e6a21752da290d12eb1c442
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2026-02-12 17:04:29 +0000

    Add a job to send a daily batch of submissions for a form
    Add a job that will send an email containing CSV(s) of submissions
    for a given form on a given day for a given mode.
    
    This job is not scheduled by anything yet.
    
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -7,4 +7,12 @@ class ApplicationJob < ActiveJob::Base
     CurrentJobLoggingAttributes.submission_reference = submission.reference
     CurrentJobLoggingAttributes.preview = submission.preview?
   end
+
+  def set_submission_batch_logging_attributes(form:, mode:)
+    CurrentJobLoggingAttributes.job_class = self.class.name
+    CurrentJobLoggingAttributes.job_id = job_id
+    CurrentJobLoggingAttributes.form_id = form.id
+    CurrentJobLoggingAttributes.form_name = form.name
+    CurrentJobLoggingAttributes.preview = mode.preview?
+  end
 end
