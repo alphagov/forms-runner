fa3707f6f42c7fc84a207b358cc17541f3e59643
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-03-18 16:27:59 +0000

    Add method to get all weekly batches for a given date
    Add a method, that given a date, will return batches of submissions
    per form and mode where the form has `send_weekly_submission_batch`
    set to true for the final submission in the week.
    
    Include all submissions for the week in the batch, even if they were
    submitted for an older version of the form that did not have
    `send_weekly_submission_batch` set to true.

--- app/lib/batch_submissions_selector.rb
+++ app/lib/batch_submissions_selector.rb
@@ -16,6 +16,20 @@ class BatchSubmissionsSelector
       end
     end
 
+    def weekly_batches(time_in_week)
+      Enumerator.new do |yielder|
+        form_ids_and_modes_with_send_weekly_submission_batch(time_in_week).each do |form_id, mode|
+          submissions = Submission.for_form_and_mode(form_id, mode).in_week(time_in_week).order(created_at: :desc)
+
+          # If the send_weekly_submission_batch is true for the most recent submission, include all submissions in that
+          # week in the batch. If it is false do not return a batch for any of the submissions in that week.
+          next unless submissions.any? && submissions.first.form_document["send_weekly_submission_batch"] == true
+
+          yielder << Batch.new(form_id, mode, submissions)
+        end
+      end
+    end
+
   private
 
     def form_ids_and_modes_with_send_daily_submission_batch(date)
@@ -23,6 +37,15 @@ class BatchSubmissionsSelector
       # set to true.
       Submission.on_day(date)
                 .where("(form_document->>'send_daily_submission_batch')::boolean = true")
+                .distinct
+                .pluck(:form_id, :mode)
+    end
+
+    def form_ids_and_modes_with_send_weekly_submission_batch(begin_at)
+      # Get all form_ids and modes that have at least one submission in the week with send_weekly_submission_batch
+      # set to true.
+      Submission.in_week(begin_at)
+                .where("(form_document->>'send_weekly_submission_batch')::boolean = true")
                 .distinct
                 .pluck(:form_id, :mode)
     end
